c47bb6c310178fdd7f04b7f6b2ac286d475380ec
BerezinAlexander <elmario2pro@gmail.com>
2018-10-01 17:00:38 +0300

    Минимальная строка

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -2,16 +2,33 @@
 //
 
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
-	int a, b;
+	string a, b, c;
 
-	cin >> a >> b;
+	cin >> a >> b >> c;
 
-	cout << a + b;
+	if (a < b) {
+		if (a < c) {
+			cout << a;
+		}
+		else {
+			cout << c;
+		}
+	}
+	else {
+		if (b < c) {
+			cout << b;
+		}
+		else {
+			cout << c;
+		}
+
+	}
 
     return 0;
 }
