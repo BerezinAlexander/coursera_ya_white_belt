67dc81946b4e49b0b111feed0fdbdebe2be8234a
BerezinAlexander <elmario2pro@gmail.com>
2018-10-01 16:50:19 +0300

    вывод А + Б

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -1,11 +1,18 @@
 // coursera_ya.cpp: определяет точку входа для консольного приложения.
 //
 
-#include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
 int main()
 {
+	int a, b;
+
+	cin >> a >> b;
+
+	cout << a + b;
+
     return 0;
 }
 
