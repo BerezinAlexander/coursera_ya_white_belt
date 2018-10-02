ea3d7812aaaa015b0334107d01060619a67ba720
BerezinAlexander <elmario2pro@gmail.com>
2018-10-02 15:09:39 +0300

    Отбор палиндромов

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -25,6 +25,17 @@ bool IsPalindrom(string text)
 	return true;
 }
 
+vector<string> PalindromFilter(vector<string> words, int minLength)
+{
+	vector<string> result;
+	for (auto& word : words) {
+		if (IsPalindrom(word) && word.size() >= minLength) {
+			result.push_back(word);
+		}
+	}
+	return result;
+}
+
 //int main()
 //{
 //	string text;
