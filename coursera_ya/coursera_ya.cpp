45985b2d7eb0cae85c96dc9417560c754b8f40d2
BerezinAlexander <elmario2pro@gmail.com>
2018-10-02 12:51:21 +0300

    Наибольший общий делитель

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -6,78 +6,102 @@
 #include <cmath>
 #include <set>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
+vector<int> reshetoEratosfena(int val)
+{
+	// init
+	vector<int> arr(val + 1);
+	for (int i = 0; i < val + 1; i++) {
+		arr[i] = i;
+	}
+	arr[1] = 0;
+	
+	vector<int> result;
+	int p = 2;
+	while (p <= val) {
+		if (arr[p] != 0) {
+			result.push_back(arr[p]);
+			for (int j = p; j < val + 1; j += p) {
+				arr[j] = 0;
+			}
+		}
+		p += 1;
+	}
+
+	return result;
+}
+
+vector<int> mnoj(vector<int>& simple, int x)
+{
+	vector<int> res;
+	for (const auto val : simple) {
+		if (val > x)
+			break;
+		while (x % val == 0) {
+			res.push_back(val);
+			x /= val;
+		}
+	}
+	return res;
+}
+
 int main()
 {
-	//int a, b;
-	//cin >> a >> b;
+	int a, b;
+	cin >> a >> b;
 
-	//if(a == b)
-	//	cout << a << endl;
-	//else {
+	while (a > 0 && b > 0)
+	{
+		if (a > b)
+			a = a % b;
+		else
+			b = b % a;
+	}
 
-	//	vector<int> va;
-	//	for (int i = 2; a != 1; i++) {
-	//		while (a % i == 0) {
-	//			va.push_back(i);
-	//			a /= i;
-	//		}
-	//	}
+	cout << a + b << endl;
+	/*
+	if (a == b)
+		cout << a << endl;
+	else {
+		vector<int> simple( reshetoEratosfena(max(a, b)) );
 
-	//	vector<int> vb;
-	//	for (int i = 2; b != 1; i++) {
-	//		while (b % i == 0) {
-	//			vb.push_back(i);
-	//			b /= i;
-	//		}
-	//	}
-
-	//	vector<int> both;
-	//	for (auto& val : va) {
-	//		for (auto& val2 : vb) {
-	//			if (val == val2) {
-	//				both.push_back(val);
-	//				val2 = -1;
-	//				break;
-	//			}
-	//		}
-	//	}
-
-	//	int mult = 1;
-	//	for (auto val : both) {
-	//		mult *= val;
-	//	}
-
-	//	cout << mult << endl;
-	//}
-	
-	
-	string a;
-	cin >> a;
-
-	bool fl = false;
-	int pos = -1;
-	for (int i = 0; i < a.length(); i++) {
-		if (a[i] == 'f') {
-			if (!fl) {
-				fl = true;
-			}else{
-				pos = i;
+		for (auto val : simple) {
+			if (a % val == 0 && b % val == 0) {
+				cout << val << endl;
 				break;
 			}
 		}
+
+		vector<int> va = mnoj(simple, a);
+		vector<int> vb = mnoj(simple, b);
+
+		vector<int> both;
+		for (auto& val : va) {
+			for (auto& val2 : vb) {
+				if (val == val2) {
+					both.push_back(val);
+					val2 = -1;
+					break;
+				}
+			}
+		}
+
+		int mult = 1;
+		for (auto val : both) {
+			mult *= val;
+		}
+
+		cout << mult << endl;
 	}
+	*/
 
-	if(!fl)
-		cout << "-2" << endl;
-	else if(fl && pos == -1)
-		cout << "-1" << endl;
-	else
-		cout << pos << endl;
+#ifdef _MSC_VER
+	system("pause");
+#endif
 
-	//system("pause");
     return 0;
 }
 
