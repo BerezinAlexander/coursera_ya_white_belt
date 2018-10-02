0475edb6a972e3a5bd954af6eda8fe890bea7ffd
BerezinAlexander <elmario2pro@gmail.com>
2018-10-02 11:13:10 +0300

    Четные числа

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -7,33 +7,19 @@
 
 using namespace std;
 
-void calc(float n, float a, float b, float x, float y)
+void calc(int a, int b)
 {
-	if (n > b)
-		cout << n - (n * y / 100.) << endl;
-	else if (n > a)
-		cout << n - (n * x / 100.) << endl;
-	else
-		cout << n << endl;
+	for (int i = a; i <= b; i++) {
+		if (i % 2 == 0)
+			cout << i << " ";
+	}
 }
-
-//void calc(int a, int b)
-//{
-//	for (int i = a; i <= b; i++) {
-//		if (i % 2 == 0)
-//			cout << i << " ";
-//	}
-//}
 
 int main()
 {
-	float n, a, b, x, y;
-	cin >> n >> a >> b >> x >> y;
-	calc(n, a, b, x, y);
-
-	//int a, b;
-	//cin >> a >> b;
-	//calc(a, b);
+	int a, b;
+	cin >> a >> b;
+	calc(a, b);
 
 	system("pause");
     return 0;
