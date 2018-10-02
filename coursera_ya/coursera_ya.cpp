3a3fb703bbde5e4dce45eb741cc41761dfa71184
BerezinAlexander <elmario2pro@gmail.com>
2018-10-02 16:08:04 +0300

    Средняя температура

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -10,28 +10,38 @@
 
 using namespace std;
 
-vector<int> Reversed(const vector<int>& v)
+int main()
 {
-	vector<int> v2(v);
-	reverse(v2.begin(), v2.end());
-	return v2;
+	int n;
+	cin >> n;
+
+	int mid = 0;
+	vector<int> vec(n);
+	for (auto& v : vec) {
+		cin >> v;
+		mid += v;
+	}
+	mid /= n;
+
+	vector<int> result;
+	int count = 0;
+	for (auto v : vec) {
+		if (v > mid) {
+			result.push_back(count);
+		}
+		count++;
+	}
+
+	cout << result.size() << endl;
+	for (auto& v : result) {
+		cout << v << " ";
+	}
+	cout << endl;
+
+#ifdef _MSC_VER
+	system("pause");
+#endif
+
+    return 0;
 }
 
-//int main()
-//{
-//	const vector<int> numbers = { 1, 2, 3, 4, 5 };
-//	for (auto x : numbers) { cout << x << " "; }
-//
-//	string text;
-//	cin >> text;
-//
-//	cout << boolalpha << IsPalindrom(text) << endl;
-//
-//
-//#ifdef _MSC_VER
-//	system("pause");
-//#endif
-//
-//    return 0;
-//}
-//
