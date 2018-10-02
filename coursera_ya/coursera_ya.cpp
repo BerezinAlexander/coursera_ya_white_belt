0924c91702517cb51d230c1aae5ee5b4045b23aa
BerezinAlexander <elmario2pro@gmail.com>
2018-10-02 14:35:39 +0300

    Двоичные числа

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -10,93 +10,25 @@
 
 using namespace std;
 
-vector<int> reshetoEratosfena(int val)
-{
-	// init
-	vector<int> arr(val + 1);
-	for (int i = 0; i < val + 1; i++) {
-		arr[i] = i;
-	}
-	arr[1] = 0;
-	
-	vector<int> result;
-	int p = 2;
-	while (p <= val) {
-		if (arr[p] != 0) {
-			result.push_back(arr[p]);
-			for (int j = p; j < val + 1; j += p) {
-				arr[j] = 0;
-			}
-		}
-		p += 1;
-	}
-
-	return result;
-}
-
-vector<int> mnoj(vector<int>& simple, int x)
-{
-	vector<int> res;
-	for (const auto val : simple) {
-		if (val > x)
-			break;
-		while (x % val == 0) {
-			res.push_back(val);
-			x /= val;
-		}
-	}
-	return res;
-}
-
 int main()
 {
-	int a, b;
-	cin >> a >> b;
+	int n;
+	cin >> n;
 
-	while (a > 0 && b > 0)
-	{
-		if (a > b)
-			a = a % b;
-		else
-			b = b % a;
+	vector<int> res;
+
+	while (n > 1) {
+		res.push_back(n % 2);
+		n /= 2;
 	}
+	res.push_back(n);
 
-	cout << a + b << endl;
-	/*
-	if (a == b)
-		cout << a << endl;
-	else {
-		vector<int> simple( reshetoEratosfena(max(a, b)) );
-
-		for (auto val : simple) {
-			if (a % val == 0 && b % val == 0) {
-				cout << val << endl;
-				break;
-			}
-		}
-
-		vector<int> va = mnoj(simple, a);
-		vector<int> vb = mnoj(simple, b);
-
-		vector<int> both;
-		for (auto& val : va) {
-			for (auto& val2 : vb) {
-				if (val == val2) {
-					both.push_back(val);
-					val2 = -1;
-					break;
-				}
-			}
-		}
-
-		int mult = 1;
-		for (auto val : both) {
-			mult *= val;
-		}
-
-		cout << mult << endl;
+	reverse(res.begin(), res.end());
+	for (auto val : res) {
+		cout << val;
 	}
-	*/
+	cout << endl;
+
 
 #ifdef _MSC_VER
 	system("pause");
