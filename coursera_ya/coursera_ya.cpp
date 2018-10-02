cfe61730af2262f27caa6b70fbce3fc0e0e6d2d0
BerezinAlexander <elmario2pro@gmail.com>
2018-10-02 12:09:07 +0300

    Второе вхождение

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -4,24 +4,80 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <set>
+#include <vector>
 
 using namespace std;
 
-void calc(int a, int b)
-{
-	for (int i = a; i <= b; i++) {
-		if (i % 2 == 0)
-			cout << i << " ";
-	}
-}
-
 int main()
 {
-	int a, b;
-	cin >> a >> b;
-	calc(a, b);
+	//int a, b;
+	//cin >> a >> b;
 
-	system("pause");
+	//if(a == b)
+	//	cout << a << endl;
+	//else {
+
+	//	vector<int> va;
+	//	for (int i = 2; a != 1; i++) {
+	//		while (a % i == 0) {
+	//			va.push_back(i);
+	//			a /= i;
+	//		}
+	//	}
+
+	//	vector<int> vb;
+	//	for (int i = 2; b != 1; i++) {
+	//		while (b % i == 0) {
+	//			vb.push_back(i);
+	//			b /= i;
+	//		}
+	//	}
+
+	//	vector<int> both;
+	//	for (auto& val : va) {
+	//		for (auto& val2 : vb) {
+	//			if (val == val2) {
+	//				both.push_back(val);
+	//				val2 = -1;
+	//				break;
+	//			}
+	//		}
+	//	}
+
+	//	int mult = 1;
+	//	for (auto val : both) {
+	//		mult *= val;
+	//	}
+
+	//	cout << mult << endl;
+	//}
+	
+	
+	string a;
+	cin >> a;
+
+	bool fl = false;
+	int pos = -1;
+	for (int i = 0; i < a.length(); i++) {
+		if (a[i] == 'f') {
+			if (!fl) {
+				fl = true;
+			}else{
+				pos = i;
+				break;
+			}
+		}
+	}
+
+	if(!fl)
+		cout << "-2" << endl;
+	else if(fl && pos == -1)
+		cout << "-1" << endl;
+	else
+		cout << pos << endl;
+
+	//system("pause");
     return 0;
 }
 
