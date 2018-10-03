eb6b6eda6fb7c0d6d10eb8a3968faa2abaf8366e
BerezinAlexander <elmario2pro@gmail.com>
2018-10-03 15:16:04 +0300

    Автобусные остановки — 1

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -16,61 +16,68 @@ int main()
 	int num_q;
 	cin >> num_q;
 
-	map<string, string> countries; // <country, capital>
+	map<string, vector<string>> buses;
+	map<string, vector<string>> stops;
 
 	for (int i = 0; i < num_q; i++) {
 		string command;
 		cin >> command;
 
-		if (command == "CHANGE_CAPITAL") {
-			string country, capital;
-			cin >> country >> capital;
-			if (countries.count(country) == 0)
-				cout << "Introduce new country " << country << " with capital " << capital << endl;
-			else {
-				if (countries[country] == capital)
-					cout << "Country " << country << " hasn't changed its capital" << endl;
-				else 
-					cout << "Country " << country << " has changed its capital from " 
-					<< countries[country] << " to " << capital << endl;
-			}
-			countries[country] = capital;
+		if (command == "NEW_BUS") {
+			string bus;
+			int stop_count;
+			cin >> bus >> stop_count;
+			vector<string> cur_stops(stop_count);
+			for (auto& stop : cur_stops) {
+				cin >> stop;
+				stops[stop].push_back(bus);
+			}			
+			buses[bus] = move(cur_stops);
 		}
-		else if (command == "RENAME") {
-			string old_country, new_country;
-			cin >> old_country >> new_country;
+		else if (command == "BUSES_FOR_STOP") {
+			string stop;
+			cin >> stop;
 
-			if (countries.count(new_country) != 0 ||
-				countries.count(old_country) == 0 ||
-				old_country == new_country)
-			{
-				cout << "Incorrect rename, skip" << endl;
-			}
+			if (stops.count(stop) == 0)
+				cout << "No stop" << endl;
 			else {
-				cout << "Country " << old_country << " with capital " << countries[old_country] 
-					<< " has been renamed to " << new_country << endl;
-				countries[new_country] = countries[old_country];
-				countries.erase(old_country);
-			}
-		}
-		else if (command == "ABOUT") {
-			string country;
-			cin >> country;
-
-			if (countries.count(country) == 0)
-				cout << "Country " << country << " doesn't exist" << endl;
-			else {
-				cout << "Country " << country << " has capital " << countries[country] << endl;
-			}
-		}
-		else if (command == "DUMP") {
-			if (countries.empty())
-				cout << "There are no countries in the world" << endl;
-			else {
-				for (auto c : countries) {
-					cout << c.first << "/" << c.second << " ";
+				for (auto& bus : stops[stop]) {
+					cout << bus << " ";
 				}
 				cout << endl;
+			}
+		}
+		else if (command == "STOPS_FOR_BUS") {
+			string bus;
+			cin >> bus;
+
+			if (buses.count(bus) == 0)
+				cout << "No bus" << endl;
+			else {
+				for (auto& stop : buses[bus]) {
+					cout << "Stop " << stop << ": ";
+					for (auto& cur_bus : stops[stop]) {
+						if(cur_bus != bus)
+							cout << cur_bus << " ";
+						else if (cur_bus == bus && stops[stop].size() == 1) {
+							cout << "no interchange";
+						}
+					}
+					cout << endl;
+				}
+			}
+		}
+		else if (command == "ALL_BUSES") {
+			if (buses.empty())
+				cout << "No buses" << endl;
+			else {
+				for (auto& p : buses) {
+					cout << "Bus " << p.first << ": ";
+					for (auto& cur_stop : buses[p.first]) {
+						cout << cur_stop << " ";
+					}
+					cout << endl;
+				}
 			}
 		}
 
