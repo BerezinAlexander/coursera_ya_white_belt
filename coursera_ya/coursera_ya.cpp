8ef05a617798b1bd6ddb53a8184f0101a56ba7f6
BerezinAlexander <elmario2pro@gmail.com>
2018-10-03 16:23:33 +0300

    Синонимы

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -20,18 +20,39 @@ set<string> BuildMapValuesSet(const map<int, string>& m) {
 
 int main()
 {
-	set<string> values = BuildMapValuesSet({
-		{ 1, "odd" },
-		{ 2, "even" },
-		{ 3, "odd" },
-		{ 4, "even" },
-		{ 5, "odd" }
-	});
+	int num_q;
+	cin >> num_q;
 
-	for (const string& value : values) {
-		cout << value << endl;
+	map<string, set<string>> m;
+
+	for (int i = 0; i < num_q; i++) {
+		string command;
+		cin >> command;
+
+		if (command == "ADD") {
+			string word1, word2;
+			cin >> word1 >> word2;
+
+			m[word1].insert(word2);
+			m[word2].insert(word1);
+		}
+		else if (command == "COUNT") {
+			string word;
+			cin >> word;
+
+			cout << m[word].size() << endl;
+		}
+		else if (command == "CHECK") {
+			string word1, word2;
+			cin >> word1 >> word2;
+
+			if (m[word1].count(word2) == 0)
+				cout << "NO";
+			else
+				cout << "YES";
+			cout << endl;
+		}
 	}
-
 
 #ifdef _MSC_VER
 	system("pause");
