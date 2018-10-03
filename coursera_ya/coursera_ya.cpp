99a8fa1ced4be0e42243656dd929c447ae825776
BerezinAlexander <elmario2pro@gmail.com>
2018-10-03 12:53:43 +0300

    Справочник столиц

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -11,25 +11,69 @@
 
 using namespace std;
 
-map<char, int> BuildCharCounters(const string& word)
-{
-	map<char, int> result;
-	for (char c : word) {
-		result[c]++;
-	}
-	return result;
-}
-
 int main()
 {
-	int num_pair;
-	cin >> num_pair;
+	int num_q;
+	cin >> num_q;
 
-	for (int i = 0; i < num_pair; i++) {
-		string s1, s2;
-		cin >> s1 >> s2;
+	map<string, string> countries; // <country, capital>
 
-		cout << ((BuildCharCounters(s1) == BuildCharCounters(s2)) ? "YES\n" : "NO\n");
+	for (int i = 0; i < num_q; i++) {
+		string command;
+		cin >> command;
+
+		if (command == "CHANGE_CAPITAL") {
+			string country, capital;
+			cin >> country >> capital;
+			if (countries.count(country) == 0)
+				cout << "Introduce new country " << country << " with capital " << capital << endl;
+			else {
+				if (countries[country] == capital)
+					cout << "Country " << country << " hasn't changed its capital" << endl;
+				else 
+					cout << "Country " << country << " has changed its capital from " 
+					<< countries[country] << " to " << capital << endl;
+			}
+			countries[country] = capital;
+		}
+		else if (command == "RENAME") {
+			string old_country, new_country;
+			cin >> old_country >> new_country;
+
+			if (countries.count(new_country) != 0 ||
+				countries.count(old_country) == 0 ||
+				old_country == new_country)
+			{
+				cout << "Incorrect rename, skip" << endl;
+			}
+			else {
+				cout << "Country " << old_country << " with capital " << countries[old_country] 
+					<< " has been renamed to " << new_country << endl;
+				countries[new_country] = countries[old_country];
+				countries.erase(old_country);
+			}
+		}
+		else if (command == "ABOUT") {
+			string country;
+			cin >> country;
+
+			if (countries.count(country) == 0)
+				cout << "Country " << country << " doesn't exist" << endl;
+			else {
+				cout << "Country " << country << " has capital " << countries[country] << endl;
+			}
+		}
+		else if (command == "DUMP") {
+			if (countries.empty())
+				cout << "There are no countries in the world" << endl;
+			else {
+				for (auto c : countries) {
+					cout << c.first << "/" << c.second << " ";
+				}
+				cout << endl;
+			}
+		}
+
 	}
 
 
