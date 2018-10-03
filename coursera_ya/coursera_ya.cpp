9ae3d72adb69a531f1b7ec78e04cde2b9028e75a
BerezinAlexander <elmario2pro@gmail.com>
2018-10-03 16:30:24 +0300

    Автобусные остановки — 3

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -11,53 +11,39 @@
 
 using namespace std;
 
-set<string> BuildMapValuesSet(const map<int, string>& m) {
-	set<string> s;
-	for (const auto& p : m)
-		s.insert(p.second);
-	return s;
-}
-
 int main()
 {
 	int num_q;
 	cin >> num_q;
 
-	map<string, set<string>> m;
+	int count_bus = 0;
+	map<set<string>, int> buses; // <stops, num_bus>
 
 	for (int i = 0; i < num_q; i++) {
-		string command;
-		cin >> command;
+		int count;
+		cin >> count;
 
-		if (command == "ADD") {
-			string word1, word2;
-			cin >> word1 >> word2;
-
-			m[word1].insert(word2);
-			m[word2].insert(word1);
+		set<string> stops;
+		for (int i = 0; i < count; i++) {
+			string stop;
+			cin >> stop;
+			stops.insert(stop);
 		}
-		else if (command == "COUNT") {
-			string word;
-			cin >> word;
 
-			cout << m[word].size() << endl;
+		if (buses.count(stops) == 0) {
+			buses[stops] = ++count_bus;
+			cout << "New bus ";
 		}
-		else if (command == "CHECK") {
-			string word1, word2;
-			cin >> word1 >> word2;
-
-			if (m[word1].count(word2) == 0)
-				cout << "NO";
-			else
-				cout << "YES";
-			cout << endl;
+		else {
+			cout << "Already exists for ";
 		}
+		cout << buses[stops] << endl;
 	}
+
 
 #ifdef _MSC_VER
 	system("pause");
 #endif
 
-    return 0;
+	return 0;
 }
-
