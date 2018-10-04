63a4842a93c19e98cb47e38b0f4f6a7cfe7d7fd0
BerezinAlexander <elmario2pro@gmail.com>
2018-10-04 16:09:41 +0300

    Обратимая функция с умножением и делением

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -11,65 +11,109 @@
 
 using namespace std;
 
-struct Specialization
-{
-	string str;
-
-	explicit Specialization(const string& s)
-		: str(s)
+class FunctionParts {
+public:
+	FunctionParts(char new_operation, double new_value)
+		: operation(new_operation), value(new_value)
 	{}
-
-	string Value() const {
-		return str;
+	double Apply(double source_value) const {
+		if (operation == '+') {
+			return source_value + value;
+		}
+		else if (operation == '-') {
+			return source_value - value;
+		}
+		else if (operation == '*') {
+			return source_value * value;
+		}
+		else if (operation == '/') {
+			return source_value / value;
+		}
 	}
+	void Invert() {
+		if (operation == '+') {
+			operation = '-';
+		}
+		else if (operation == '-') {
+			operation = '+';
+		}
+		else if (operation == '*') {
+			operation = '/';
+		}
+		else if (operation == '/') {
+			operation = '*';
+		}
+	}
+private:
+	char operation;
+	double value;
 };
 
-struct Course
-{
-	string str;
-
-	explicit Course(const string& c)
-		: str(c)
-	{}
-	string Value() const {
-		return str;
+class Function {
+public:
+	void AddPart(const char operation, const double value) {
+		parts.emplace_back( operation, value );
 	}
+	double Apply(double value) const {
+		for (auto& part : parts) {
+			value = part.Apply(value);
+		}
+		return value;
+	}
+	void Invert() {
+		for (auto& part : parts) {
+			part.Invert();
+		}
+		reverse(parts.begin(), parts.end());
+	}
+private:
+	vector<FunctionParts> parts;
 };
 
-struct Week
-{
-	string str;
-
-	explicit Week(const string& w){
-		str = w;
-	}
-	string Value() const {
-		return str;
-	}
-};
-
-struct LectureTitle {
-	string specialization;
-	string course;
-	string week;
-
-	LectureTitle(const Specialization& sp, const Course& c, const Week& w)
-		: specialization(sp.Value()), course(c.Value()), week(w.Value())
-	{
-	}
-};
-
-int main()
-{
-	LectureTitle title(
-		Specialization("C++"),
-		Course("White belt"),
-		Week("4th")
-	);
-
-#ifdef _MSC_VER
-	system("pause");
-#endif
-
-	return 0;
-}
+//struct Image {
+//	double quality;
+//	double freshness;
+//	double rating;
+//};
+//
+//struct Params {
+//	double a;
+//	double b;
+//	double c;
+//};
+//
+//Function MakeWeightFunction(const Params& params,
+//	const Image& image) {
+//	Function function;
+//	function.AddPart('*', params.a);
+//	function.AddPart('-', image.freshness * params.b);
+//	function.AddPart('+', image.rating * params.c);
+//	return function;
+//}
+//
+//double ComputeImageWeight(const Params& params, const Image& image) {
+//	Function function = MakeWeightFunction(params, image);
+//	return function.Apply(image.quality);
+//}
+//
+//double ComputeQualityByWeight(const Params& params,
+//	const Image& image,
+//	double weight) {
+//	Function function = MakeWeightFunction(params, image);
+//	function.Invert();
+//	return function.Apply(weight);
+//}
+//
+//int main()
+//{
+//	Image image = { 10, 2, 6 };
+//	Params params = { 4, 2, 6 };
+//	cout << ComputeImageWeight(params, image) << endl;
+//	cout << ComputeQualityByWeight(params, image, 52) << endl;//
+//
+//#ifdef _MSC_VER
+//	system("pause");
+//#endif
+//
+//	return 0;
+//}
