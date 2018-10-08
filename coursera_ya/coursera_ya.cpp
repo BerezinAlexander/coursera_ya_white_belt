b6f226929fd675eba36a903bf88159c2adf7ca32
BerezinAlexander <elmario2pro@gmail.com>
2018-10-08 14:56:18 +0300

    Работа с файлами

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -8,112 +8,26 @@
 #include <vector>
 #include <algorithm>
 #include <map>
+#include <fstream>
 
 using namespace std;
 
-class FunctionParts {
-public:
-	FunctionParts(char new_operation, double new_value)
-		: operation(new_operation), value(new_value)
-	{}
-	double Apply(double source_value) const {
-		if (operation == '+') {
-			return source_value + value;
-		}
-		else if (operation == '-') {
-			return source_value - value;
-		}
-		else if (operation == '*') {
-			return source_value * value;
-		}
-		else if (operation == '/') {
-			return source_value / value;
-		}
-	}
-	void Invert() {
-		if (operation == '+') {
-			operation = '-';
-		}
-		else if (operation == '-') {
-			operation = '+';
-		}
-		else if (operation == '*') {
-			operation = '/';
-		}
-		else if (operation == '/') {
-			operation = '*';
-		}
-	}
-private:
-	char operation;
-	double value;
-};
+int main()
+{
+	ifstream input("input.txt");
+	ofstream output("output.txt");
 
-class Function {
-public:
-	void AddPart(const char operation, const double value) {
-		parts.emplace_back( operation, value );
-	}
-	double Apply(double value) const {
-		for (auto& part : parts) {
-			value = part.Apply(value);
+	if (input.is_open()) {
+		string line;
+		while (getline(input, line)) {
+			//cout << line << endl;
+			output << line << endl;
 		}
-		return value;
 	}
-	void Invert() {
-		for (auto& part : parts) {
-			part.Invert();
-		}
-		reverse(parts.begin(), parts.end());
-	}
-private:
-	vector<FunctionParts> parts;
-};
 
-//struct Image {
-//	double quality;
-//	double freshness;
-//	double rating;
-//};
-//
-//struct Params {
-//	double a;
-//	double b;
-//	double c;
-//};
-//
-//Function MakeWeightFunction(const Params& params,
-//	const Image& image) {
-//	Function function;
-//	function.AddPart('*', params.a);
-//	function.AddPart('-', image.freshness * params.b);
-//	function.AddPart('+', image.rating * params.c);
-//	return function;
-//}
-//
-//double ComputeImageWeight(const Params& params, const Image& image) {
-//	Function function = MakeWeightFunction(params, image);
-//	return function.Apply(image.quality);
-//}
-//
-//double ComputeQualityByWeight(const Params& params,
-//	const Image& image,
-//	double weight) {
-//	Function function = MakeWeightFunction(params, image);
-//	function.Invert();
-//	return function.Apply(weight);
-//}
-//
-//int main()
-//{
-//	Image image = { 10, 2, 6 };
-//	Params params = { 4, 2, 6 };
-//	cout << ComputeImageWeight(params, image) << endl;
-//	cout << ComputeQualityByWeight(params, image, 52) << endl;//
-//
-//#ifdef _MSC_VER
-//	system("pause");
-//#endif
-//
-//	return 0;
-//}
+#ifdef _MSC_VER
+	system("pause");
+#endif
+
+	return 0;
+}
