4e8719728d8e93682785b5f6bffdf932db0e3536
BerezinAlexander <elmario2pro@gmail.com>
2018-11-12 17:21:38 +0300

    Реализация класса Rational

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -13,59 +13,119 @@
 
 using namespace std;
 
-struct Student {
-	string firstName;
-	string lastName;
-	int day;
-	int month;
-	int year;
+class Rational {
+public:
+	Rational() : _numerator(0), _denominator(1) {};
+	Rational(int numerator, int denominator){
+		if (numerator == 0)
+			denominator = 1;
+		
+		if (numerator < 0 && denominator < 0){ 
+			numerator = abs(numerator);
+			denominator = abs(denominator);
+		}
+		else if (denominator < 0) {
+			numerator -= 2 * numerator;
+			denominator = abs(denominator);
+		}
+
+		int nod = NOD(numerator, denominator);
+		_numerator = numerator / nod;
+		_denominator = denominator / nod;
+	};
+
+	int Numerator() const { return _numerator; };
+	int Denominator() const { return _denominator; };
+
+private:
+	int _numerator, _denominator;
+
+	int NOD(int a_, int b_) {
+		// пока оба числа положительны, будем их уменьшать, не меняя их НОД
+		int a = abs(a_);
+		int b = abs(b_);
+
+		while (a > 0 && b > 0) {
+
+			// возьмём большее из чисел и заменим его остатком от деления на второе
+			// действительно, если a и b делятся на x, то a - b и b делятся на x
+			// тогда и a % b и b делятся на x, так что можно a заменить на a % b
+			if (a > b) {
+				a %= b;
+			}
+			else {
+				b %= a;
+			}
+
+		}
+
+		return a + b;
+	}
 };
 
-int main()
-{	
-	int n, m;
-
-	cin >> n;
-
-	vector<Student> students(n);
-
-	for (int i = 0; i < n; i++) {
-		Student& stud = students[i];
-		cin >> stud.firstName >> stud.lastName >> stud.day
-			>> stud.month >> stud.year;
-	}
-
-	cin >> m;
-
-	string req;
-	int num;
-	for (int i = 0; i < m; i++) {
-		cin >> req >> num;
-
-		if (num <= 0 || num > n) {
-			cout << "bad request" << endl;
-			continue;
-		}
-		
-		--num;
-
-		if (req == "name") {
-			cout << students[num].firstName << " " << students[num].lastName 
-				<< endl;
-		}
-		else if (req == "date") {
-			cout << students[num].day << "." << students[num].month << "." 
-				<< students[num].year << endl;
-		}
-		else {
-			cout << "bad request" << endl;
+int main() {
+	{
+		const Rational r(-3, -10);
+		if (r.Numerator() != 3 || r.Denominator() != 10) {
+			cout << "Rational(-3, -10) != 3/10" << endl;
+			return 1;
 		}
 	}
 
-/*
+	{
+		const Rational r(3, 10);
+		if (r.Numerator() != 3 || r.Denominator() != 10) {
+			cout << "Rational(3, 10) != 3/10" << endl;
+			return 1;
+		}
+	}
+
+	{
+		const Rational r(8, 12);
+		if (r.Numerator() != 2 || r.Denominator() != 3) {
+			cout << "Rational(8, 12) != 2/3" << endl;
+			return 2;
+		}
+	}
+
+	{
+		const Rational r(-4, 6);
+		if (r.Numerator() != -2 || r.Denominator() != 3) {
+			cout << "Rational(-4, 6) != -2/3" << endl;
+			return 3;
+		}
+	}
+
+	{
+		const Rational r(4, -6);
+		if (r.Numerator() != -2 || r.Denominator() != 3) {
+			cout << "Rational(4, -6) != -2/3" << endl;
+			return 3;
+		}
+	}
+
+	{
+		const Rational r(0, 15);
+		if (r.Numerator() != 0 || r.Denominator() != 1) {
+			cout << "Rational(0, 15) != 0/1" << endl;
+			return 4;
+		}
+	}
+
+	{
+		const Rational defaultConstructed;
+		if (defaultConstructed.Numerator() != 0 || defaultConstructed.Denominator() != 1) {
+			cout << "Rational() != 0/1" << endl;
+			return 5;
+		}
+	}
+
+	cout << "OK" << endl;
+
+
 #ifdef _MSC_VER
 	system("pause");
-#endif*/
+#endif
 
 	return 0;
 }
