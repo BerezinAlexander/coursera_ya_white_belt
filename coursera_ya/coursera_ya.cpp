3ef32918739db8c906e1cae7872ed13204491055
BerezinAlexander <elmario2pro@gmail.com>
2018-11-15 09:44:52 +0300

    Revert "Merge commit '7aede86a862b88e1ba6f1594bbb1b81767693f66' into yellbelt-fname-and-lname"
    This reverts commit 586bc528c89c9c0c0cfdb90cc3b15b9f955db01c, reversing
    changes made to ef73934e2fee3c5d4dea82fd734dee79509edfbc.

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -2,59 +2,81 @@
 //
 
 #include <iostream>
-#include <fstream>
 #include <string>
 #include <cmath>
 #include <set>
 #include <vector>
 #include <algorithm>
 #include <map>
-#include <iomanip>
 
 using namespace std;
 
+class Person {
+public:
+	void ChangeFirstName(int year, const string& first_name) {
+		// добавить факт изменения имени на first_name в год year
+		history_first[year] = first_name;
+	}
+	void ChangeLastName(int year, const string& last_name) {
+		// добавить факт изменения фамилии на last_name в год year
+		history_last[year] = last_name;
+	}
+	string GetFullName(int year) {
+		// получить имя и фамилию по состоянию на конец года year
+		auto first = find_if(history_first.rbegin(), history_first.rend(), 
+			[&year](const auto& p) {
+				return p.first <= year;
+			}
+		);
+		auto last = find_if(history_last.rbegin(), history_last.rend(),
+			[&year](const auto& p) {
+				return p.first <= year;
+			}
+		);
+
+		if (first == history_first.rend() && last == history_last.rend()) {
+			return "Incognito";
+		}
+		else if (first != history_first.rend() && last == history_last.rend()) {
+			return first->second + " with unknown last name";
+		}
+		else if (first == history_first.rend() && last != history_last.rend()) {
+			return last->second + " with unknown first name";
+		}
+		else {
+			return first->second + " " + last->second;
+		}
+}
+private:
+	// приватные поля
+	map<int, string> history_first;
+	map<int, string> history_last;
+};
+
+
 int main()
 {
-	ifstream input("input.txt");
+	Person person;
 
-	if (!input) {
-		//cout << "File not open!" << endl;
-		return 0;
+	person.ChangeFirstName(1965, "Polina");
+	person.ChangeLastName(1967, "Sergeeva");
+	for (int year : {1900, 1965, 1990}) {
+		cout << person.GetFullName(year) << endl;
 	}
 
-	int n, m;
-
-	input >> n >> m;
-
-	vector<vector<int>> table;
-	table.resize(n);
-	for (auto& line : table) {
-		line.resize(m);
+	person.ChangeFirstName(1970, "Appolinaria");
+	for (int year : {1969, 1970}) {
+		cout << person.GetFullName(year) << endl;
 	}
 
-	auto itLine = table.begin();
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			input >> (*itLine)[j];
-			if(j < m-1)
-				input.ignore(1);
-		}
-		itLine++;
+	person.ChangeLastName(1968, "Volkova");
+	for (int year : {1969, 1970}) {
+		cout << person.GetFullName(year) << endl;
 	}
 
-	for (auto it = table.begin(); it != table.end(); it++) {
-		for (auto itItem = it->begin(); itItem != it->end(); itItem++) {
-			if (itItem != it->begin())
-				cout << " ";
-			cout << setw(10) << *itItem;
-		}
-		if(it < prev( table.end() ) )
-			cout << endl;
-	}
-/*
 #ifdef _MSC_VER
 	system("pause");
-#endif*/
+#endif
 
 	return 0;
 }
