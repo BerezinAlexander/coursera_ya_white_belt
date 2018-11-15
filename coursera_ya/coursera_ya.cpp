ead989eba9726f099e4b93bec848e2cda6c58639
BerezinAlexander <elmario2pro@gmail.com>
2018-11-15 19:00:35 +0300

    Группировка строк по префиксу

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -11,80 +11,53 @@
 
 using namespace std;
 
-string FindName(const map<int, string>& history, const int year)
+template <typename RandomIt>
+pair<RandomIt, RandomIt> FindStartsWith(
+	RandomIt range_begin, RandomIt range_end,
+	const string& prefix)
 {
-	string result;
-	auto it = history.upper_bound(year);
-	if (it != history.begin()) {
-		--it;
-		result = it->second;
-	}
-	return result;
+	auto comp1 = [](const string& str, const string& pref) {
+		if (!str.empty())
+			return str.substr(0, pref.size()) < pref;
+		else
+			return false;
+	};
+
+	auto comp2 = [](const string& pref, const string& str) {
+		if (!str.empty())
+			return pref < str.substr(0, pref.size());
+		else
+			return false;
+	};
+
+	auto itLower = std::lower_bound(range_begin, range_end, prefix, comp1);
+	auto itUpper = std::upper_bound(range_begin, range_end, prefix, comp2);
+
+	return std::make_pair(itLower,itUpper);
+
+	//return equal_range(range_begin, range_end, prefix, Comp);
 }
-
-string FormationResult(const string& first_name, const string& last_name)
-{
-	string result;
-
-	if (first_name.empty() && last_name.empty())
-		result = "Incognito";
-	else if (first_name.empty())
-		result = last_name + " with unknown first name";
-	else if (last_name.empty())
-		result = first_name + " with unknown last name";
-	else
-		result = first_name + " " + last_name;
-
-	return result;
-}
-
-class Person {
-public:
-	Person() = default;
-
-	void ChangeFirstName(int year, const string& first_name) {
-		history_first[year] = first_name;
-	}
-
-	void ChangeLastName(int year, const string& last_name) {
-		history_last[year] = last_name;
-	}
-
-	string GetFullName(int year) const {
-		// получить имя и фамилию по состоянию на конец года year
-
-		string first_name = FindName(history_first, year);
-		string last_name = FindName(history_last, year);
-
-		return FormationResult(first_name, last_name);
-	}
-
-private:
-	// приватные поля
-	map<int, string> history_first;
-	map<int, string> history_last;
-};
-
 
 int main()
 {
-	Person person;
+	const vector<string> sorted_strings = { "moscow", "motovilikha", "murmansk" };
 
-	person.ChangeFirstName(1965, "Polina");
-	person.ChangeLastName(1967, "Sergeeva");
-	for (int year : {1900, 1965, 1990}) {
-		cout << person.GetFullName(year) << endl;
+	const auto mo_result =
+		FindStartsWith(begin(sorted_strings), end(sorted_strings), "mo");
+	for (auto it = mo_result.first; it != mo_result.second; ++it) {
+		cout << *it << " ";
 	}
+	cout << endl;
 
-	person.ChangeFirstName(1970, "Appolinaria");
-	for (int year : {1969, 1970}) {
-		cout << person.GetFullName(year) << endl;
-	}
+	const auto mt_result =
+		FindStartsWith(begin(sorted_strings), end(sorted_strings), "mt");
+	cout << (mt_result.first - begin(sorted_strings)) << " " <<
+		(mt_result.second - begin(sorted_strings)) << endl;
 
-	person.ChangeLastName(1968, "Volkova");
-	for (int year : {1969, 1970}) {
-		cout << person.GetFullName(year) << endl;
-	}
+	const auto na_result =
+		FindStartsWith(begin(sorted_strings), end(sorted_strings), "na");
+	cout << (na_result.first - begin(sorted_strings)) << " " <<
+		(na_result.second - begin(sorted_strings)) << endl;
 
 #ifdef _MSC_VER
 	system("pause");
