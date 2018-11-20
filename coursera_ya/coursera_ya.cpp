197a0e9767cef7a88e8c6d35f489e78ffef2a40e
BerezinAlexander <elmario2pro@gmail.com>
2018-11-20 18:25:30 +0300

    Генератор компараторов

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -1,98 +1,74 @@
-//#include "test_runner.h"
+#include "airline_ticket.h"
+#include "test_runner.h"
 
-#include <string>
-#include <vector>
-#include <iostream>
-
+#include <algorithm>
+#include <numeric>
 using namespace std;
 
-
-class Person {
-public:
-	explicit Person(const string &n, const string &t) : name(n), type(t) {}
-
-	virtual void Walk(const string &destination) const {
-		cout << type << ": " << name << " walks to: " << destination << endl;
-	};
-
-	string Log() const {
-		return type + ": " + name;
-	}
-
-	string GetName() const {
-		return name;
-	}
-
-	string GetType() const {
-		return type;
-	}
-
-protected:
-	const string name;
-	const string type;
-};
-
-class Student : public Person {
-public:
-	explicit Student(const string &name, const string &favouriteSong) : Person(name, "Student"), FavouriteSong(favouriteSong) {}
-
-	void Learn() const {
-		cout << Log() << " learns" << endl;
-	}
-
-	void Walk(const string &destination) const override {
-		cout << Log() << " walks to: " << destination << endl;
-		cout << Log() << " sings a song: " << FavouriteSong << endl;
-	}
-
-	void SingSong() const {
-		cout << Log() << " sings a song: " << FavouriteSong << endl;
-	}
-
-private:
-	const string FavouriteSong;
-};
-
-class Teacher : public Person {
-public:
-	explicit Teacher(const string &name, string &subject) : Person(name, "Teacher"), Subject(subject) {}
-
-	void Teach() const {
-		cout << Log() << " teaches: " << Subject << endl;
-	}
-
-private:
-	const string Subject;
-};
-
-class Policeman : Person {
-public:
-	explicit Policeman(const string &name) : Person(name, "Policeman") {}
-
-	void Check(const Person &t) const {
-		cout << Log() << " checks " << t.GetType() << ". " << t.GetType() << "'s name is: "
-			<< t.GetName() << endl;
-	}
-};
-
-
-void VisitPlaces(const Person &person, vector<string> places) {
-	for (const auto &p : places) {
-		person.Walk(p);
-	}
+bool operator<(const Date& lhs, const Date& rhs) {
+	return vector<int>{lhs.year, lhs.month, lhs.day} <
+		vector<int>{rhs.year, rhs.month, rhs.day};
 }
 
+bool operator==(const Date& lhs, const Date& rhs) {
+	return vector<int>{lhs.year, lhs.month, lhs.day} ==
+		vector<int>{rhs.year, rhs.month, rhs.day};
+}
+
+ostream& operator<<(ostream& os, const Date& date) {
+	os << date.year << "-" << date.month << "-" << date.day;
+	return os;
+}
+
+ostream& operator<<(ostream& os, const Time& date) {
+	os << date.hours << ":" << date.minutes;
+	return os;
+}
+
+bool operator<(const Time& lhs, const Time& rhs) {
+	return vector<int>{lhs.hours, lhs.minutes} <
+		vector<int>{rhs.hours, rhs.minutes};
+}
+
+bool operator==(const Time& lhs, const Time& rhs) {
+	return vector<int>{lhs.hours, lhs.minutes} ==
+		vector<int>{rhs.hours, rhs.minutes};
+}
+
+// Реализуйте этот макрос, а также необходимые операторы для классов Date и Time
+#define SORT_BY(field) 											\
+	[](const AirlineTicket& lhs, const AirlineTicket& rhs) {	\
+		return lhs.field < rhs.field;							\
+	}															\
+
+
+void TestSortBy() {
+	vector<AirlineTicket> tixs = {
+		{ "VKO", "AER", "Utair",{ 2018, 2, 28 },{ 17, 40 },{ 2018, 2, 28 },{ 20,  0 }, 1200 },
+		{ "AER", "VKO", "Utair",{ 2018, 3,  5 },{ 14, 15 },{ 2018, 3,  5 },{ 16, 30 }, 1700 },
+		{ "AER", "SVO", "Aeroflot",{ 2018, 3,  5 },{ 18, 30 },{ 2018, 3,  5 },{ 20, 30 }, 2300 },
+		{ "PMI", "DME", "Iberia",{ 2018, 2,  8 },{ 23, 00 },{ 2018, 2,  9 },{ 3, 30 }, 9000 },
+		{ "CDG", "SVO", "AirFrance",{ 2018, 3,  1 },{ 13, 00 },{ 2018, 3,  1 },{ 17, 30 }, 8000 },
+	};
+
+	sort(begin(tixs), end(tixs), SORT_BY(price));
+	ASSERT_EQUAL(tixs.front().price, 1200);
+	ASSERT_EQUAL(tixs.back().price, 9000);
+
+	sort(begin(tixs), end(tixs), SORT_BY(from));
+	ASSERT_EQUAL(tixs.front().from, "AER");
+	ASSERT_EQUAL(tixs.back().from, "VKO");
+
+	sort(begin(tixs), end(tixs), SORT_BY(arrival_date));
+	ASSERT_EQUAL(tixs.front().arrival_date, (Date{ 2018, 2, 9 }));
+	ASSERT_EQUAL(tixs.back().arrival_date, (Date{ 2018, 3, 5 }));
+}
 
 int main() {
-	string teacher_name = "Matt", student_name = "Ann", policeman_name = "Bob";
-	string subject = "Math", song = "We will rock you";
-	Teacher t(teacher_name, subject);
-	Student s(student_name, song);
-	Policeman p(policeman_name);
-	vector<string> places = { "Moscow", "London" };
+	TestRunner tr;
+	RUN_TEST(tr, TestSortBy);
 
-	VisitPlaces(t, places);
-	p.Check(s);
-	VisitPlaces(s, places);
-	return 0;
+#ifdef _MSC_VER
+	system("pause");
+#endif
 }
