43c00055affa0132d820e7f5eb464049ac133111
BerezinAlexander <elmario2pro@gmail.com>
2018-11-21 18:22:34 +0300

    Макрос UPDATE_FIELD

--- coursera_ya/airline_ticket.h
+++ coursera_ya/airline_ticket.h
@@ -6,22 +6,22 @@ using namespace std;
 struct Date {
 	int year, month, day;
 
-	Date& operator=(const Date& rhs) {
-		year = rhs.year;
-		month = rhs.month;
-		day = rhs.day;
-		return *this;
-	}
+	//Date& operator=(const Date& rhs) {
+	//	year = rhs.year;
+	//	month = rhs.month;
+	//	day = rhs.day;
+	//	return *this;
+	//}
 };
 
 struct Time {
 	int hours, minutes;
 
-	Time& operator=(const Time& rhs) {
-		hours = rhs.hours;
-		minutes = rhs.minutes;
-		return *this;
-	}
+	//Time& operator=(const Time& rhs) {
+	//	hours = rhs.hours;
+	//	minutes = rhs.minutes;
+	//	return *this;
+	//}
 };
 
 struct AirlineTicket {
--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -5,35 +5,120 @@
 #include <numeric>
 using namespace std;
 
+
+Date ParseDate(std::istream& is)
+{
+	string date;
+	is >> date;
+	istringstream date_stream(date);
+	bool ok = true;
+
+	int year;
+	ok = ok && (date_stream >> year);
+	ok = ok && (date_stream.peek() == '-');
+	date_stream.ignore(1);
+
+	int month;
+	ok = ok && (date_stream >> month);
+	ok = ok && (date_stream.peek() == '-');
+	date_stream.ignore(1);
+
+	int day;
+	ok = ok && (date_stream >> day);
+	ok = ok && date_stream.eof();
+
+	if (!ok) {
+		throw logic_error("Wrong date format: " + date);
+	}
+	return{ year, month, day };
+}
+
+Time ParseTime(std::istream& is)
+{
+	string stime;
+	is >> stime;
+	istringstream time_stream(stime);
+	bool ok = true;
+
+	int hours;
+	ok = ok && (time_stream >> hours);
+	ok = ok && (time_stream.peek() == ':');
+	time_stream.ignore(1);
+
+	int minutes;
+	ok = ok && (time_stream >> minutes);
+	ok = ok && time_stream.eof();
+
+	if (!ok) {
+		throw logic_error("Wrong time format: " + stime);
+	}
+	return{ hours, minutes };
+}
+
+template<class T, class Func>
+T ParseFromStr(const std::string& s) {
+	stringstream ss;
+	ss << s;
+	try {
+		return Func(ss);
+	}
+	catch (...) {
+		return T();
+	}
+}
+
+
 bool operator<(const Date& lhs, const Date& rhs) {
 	return vector<int>{lhs.year, lhs.month, lhs.day} <
 		vector<int>{rhs.year, rhs.month, rhs.day};
 }
-
 bool operator==(const Date& lhs, const Date& rhs) {
 	return vector<int>{lhs.year, lhs.month, lhs.day} ==
 		vector<int>{rhs.year, rhs.month, rhs.day};
 }
-
+bool operator!=(const Date& lhs, const Date& rhs) {
+	return vector<int>{lhs.year, lhs.month, lhs.day} !=
+		vector<int>{rhs.year, rhs.month, rhs.day};
+}
 ostream& operator<<(ostream& os, const Date& date) {
 	os << date.year << "-" << date.month << "-" << date.day;
 	return os;
 }
-
-ostream& operator<<(ostream& os, const Time& date) {
-	os << date.hours << ":" << date.minutes;
-	return os;
+istream& operator>>(istream& is, Date& date) {
+	try {
+		date = ParseDate(is);
+	}
+	catch (...) {
+	}
+	return is;
 }
+
 
 bool operator<(const Time& lhs, const Time& rhs) {
 	return vector<int>{lhs.hours, lhs.minutes} <
 		vector<int>{rhs.hours, rhs.minutes};
 }
-
 bool operator==(const Time& lhs, const Time& rhs) {
 	return vector<int>{lhs.hours, lhs.minutes} ==
 		vector<int>{rhs.hours, rhs.minutes};
 }
+bool operator!=(const Time& lhs, const Time& rhs) {
+	return vector<int>{lhs.hours, lhs.minutes} !=
+		vector<int>{rhs.hours, rhs.minutes};
+}
+ostream& operator<<(ostream& os, const Time& date) {
+	os << date.hours << ":" << date.minutes;
+	return os;
+}
+istream& operator >> (istream& is, Time& time) {
+	try {
+		time = ParseTime(is);
+	}
+	catch (...) {
+	}
+	return is;
+}
+
 
 // Реализуйте этот макрос, а также необходимые операторы для классов Date и Time
 #define SORT_BY(field) 											\
@@ -41,26 +126,30 @@ bool operator==(const Time& lhs, const Time& rhs) {
 		return lhs.field < rhs.field;							\
 	}		
 
-#define EQUAL_BY(field) 					\
-	[](const pair<string, string>& p) {		\
-		return p.first == #field;			\
-	}	
+//#define EQUAL_BY(field) 					\
+//	[](const pair<string, string>& p) {		\
+//		return p.first == #field;			\
+//	}	
 
 // Реализуйте этот макрос, а также необходимые операторы для классов Date и Time
 #define UPDATE_FIELD(ticket, field, values) {									\
-	auto it = find_if(values.begin(), values.end(), EQUAL_BY(field));			\
-	if (it != values.end())														\
-		ticket.field = it->second;	\
+	auto it = values.find(#field);	\
+	if (it != values.end()) {	\
+		istringstream is(it->second);	\
+		is >> ticket.field;	\
+	}	\
 }
 
 void TestUpdate() {
 	AirlineTicket t;
 	t.price = 0;
 
+
 	const map<string, string> updates1 = {
 		{ "departure_date", "2018-2-28" },
 		{ "departure_time", "17:40" },
 	};
+
 	UPDATE_FIELD(t, departure_date, updates1);
 	UPDATE_FIELD(t, departure_time, updates1);
 	UPDATE_FIELD(t, price, updates1);
@@ -88,7 +177,7 @@ void TestUpdate() {
 
 int main() {
 	TestRunner tr;
-	RUN_TEST(tr, TestSortBy);
+	RUN_TEST(tr, TestUpdate);
 
 #ifdef _MSC_VER
 	system("pause");
