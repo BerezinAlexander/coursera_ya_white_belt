d46a1e6ffc84ea5a89b1c3994b0a6423ae4b1b6b
BerezinAlexander <elmario2pro@gmail.com>
2018-11-22 11:44:34 +0300

    Таблица

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -1,24 +1,55 @@
 #include "test_runner.h"
 
 #include <iostream>
-#include <algorithm>
-#include <numeric>
-#include <ostream>
 #include <string>
 #include <vector>
 
 using namespace std;
 
+template <typename T>
+class Table {
+public:
+	Table(size_t col, size_t row) {
+		Resize(col, row);
+	}
 
-#define CAT2(a,b) a##b
-#define CAT(a, b) CAT2(a,b)
-#define UNIQ_ID CAT(param_, __LINE__) 
+	vector<T>& operator[](const size_t num) {
+		return table[num];
+	}
+
+	const vector<T>& operator[](const size_t num) const {
+		return table[num];
+	}
+
+	void Resize(size_t col, size_t row) {
+		table.resize(col);
+		for (auto& vec_r : table) {
+			vec_r.resize(row);
+		}
+	}
+
+	pair<size_t, size_t> Size() const {
+		return{ table.size(), (table.empty()) ? 0 : table.begin()->size() };
+	}
+
+private:
+	vector<vector<T>> table;
+};
+
+void TestTable() {
+	Table<int> t(1, 1);
+	ASSERT_EQUAL(t.Size().first, 1u);
+	ASSERT_EQUAL(t.Size().second, 1u);
+	t[0][0] = 42;
+	ASSERT_EQUAL(t[0][0], 42);
+	t.Resize(3, 4);
+	ASSERT_EQUAL(t.Size().first, 3u);
+	ASSERT_EQUAL(t.Size().second, 4u);
+}
 
 int main() {
-	int UNIQ_ID = 0;
-	string UNIQ_ID = "hello";
-	vector<string> UNIQ_ID = { "hello", "world" };
-	vector<int> UNIQ_ID = { 1, 2, 3, 4 };
+	TestRunner tr;
+	RUN_TEST(tr, TestTable);
 
 #ifdef _MSC_VER
 	system("pause");
