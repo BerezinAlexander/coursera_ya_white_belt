f3949b4f41076e05e9b0427c8fc6f2ebf6e5763e
BerezinAlexander <elmario2pro@gmail.com>
2018-11-22 19:10:57 +0300

    template

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -1,137 +1,191 @@
+#include "test_runner.h"
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <exception>
+#include <numeric>
 
 using namespace std;
 
+// Реализуйте шаблон класса Paginator
+
+template <typename Iterator>
+struct IteratorRange {
+	Iterator first, last;
+
+	Iterator begin() const {
+		return first;
+	}
+
+	Iterator end() const {
+		return last;
+	}
+
+	size_t size() const {
+		return last - first;
+	}
+};
+
 template <typename T>
-class Deque {
+IteratorRange<typename vector<T>::iterator> 
+Head(vector<T>& v, size_t top) {
+	return{ v.begin(), next( v.begin(), min(top, v.size()) ) };
+}
+
+template <typename T>
+size_t RangeSize(IteratorRange<T> r) {
+	return r.end() - r.begin();
+}
+
+template <typename Iterator>
+IteratorRange<Iterator> MakeRange(Iterator begin, Iterator end) {
+	return IteratorRange<Iterator>{begin, end};
+}
+
+template <typename Iterator>
+class Paginator {
 public:
-	Deque() = default;
-
-	bool Empty() const {
-		return vFront.empty() && vBack.empty();
-	}
-
-	size_t Size() const {
-		return (Empty()) ? 0 : (vFront.size() + vBack.size());
-	}
-
-	T& operator[](size_t index) {
-		return At(index);
-	}
-
-	const T& operator[](size_t index) const {
-		return At(index);
-	}
-
-	T& At(size_t index) {
-		if (index > (Size() - 1))
-			throw out_of_range("");
-
-		size_t pos = index;
-		if (pos >= vFront.size()) {
-			pos -= vFront.size();
-			return vBack.at(pos);
-		}
-		else {
-			return vFront.at(vFront.size() - 1 - pos);//   *(next(vFront.rbegin(), pos));
+	Paginator(Iterator begin, Iterator end, size_t page_size) {
+		size_t count = 0;
+		Iterator first;
+		for (auto it = begin; it != end; ++it) {
+			if (count == 0) {
+				first = it;
+			}else if(count == page_size || it == prev(end)){
+				pages.emplace_back(MakeRange(fisrt, it));
+			}
 		}
 	}
 
-	const T& At(size_t index) const {
-		if (index >= Size())
-			throw out_of_range("");
-
-		size_t pos = index;
-		if (pos >= vFront.size()) {
-			pos -= vFront.size();
-			return vBack.at(pos);
-		}
-		else {
-			return vFront.at(vFront.size() - 1 - pos);//   *(next(vFront.rbegin(), pos));
-		}
+	vector<IteratorRange<Iterator>>::iterator begin() const {
+		return pages.begin();
 	}
 
-	T& Front() {
-		if (Empty())
-			throw out_of_range("");
-		else if (!vFront.empty())
-			return vFront.back();
-		else
-			return vBack.front();
+	vector<IteratorRange<Iterator>>::iterator end() const {
+		return pages.end();
 	}
 
-
-	const T& Front() const {
-		if(Empty())
-			throw out_of_range("");
-		else if (!vFront.empty()) 
-			return vFront.back();
-		else 
-			return vBack.front();
-	}
-
-	T& Back() {
-		if (Empty())
-			throw out_of_range("");
-		else if (!vBack.empty())
-			return vBack.back();
-		else
-			return vFront.front();
-	}
-
-	const T& Back() const {
-		if (Empty())
-			throw out_of_range("");
-		else if (!vBack.empty())
-			return vBack.back();
-		else
-			return vFront.front();
-	}
-
-	void PushBack(const T& val) {
-		vBack.emplace_back(val);
-	}
-
-	void PushFront(const T& val) {
-		vFront.emplace_back(val);
+	size_t size() const {
+		return pages.size();
 	}
 
 private:
-	vector<T> vFront;
-	vector<T> vBack;
+	vector<IteratorRange<Iterator>> pages;
 };
 
+template <typename C>
+Paginator<C::iterator> Paginate(C& c, size_t page_size) {
+	// Реализуйте этот шаблон функции
+	Paginator<C::iterator> p(c.begin(), c.end(), page_size);
+	return p;
+}
+
+void TestPageCounts() {
+	vector<int> v(15);
+
+	ASSERT_EQUAL(Paginate(v, 1).size(), v.size());
+	ASSERT_EQUAL(Paginate(v, 3).size(), 5u);
+	ASSERT_EQUAL(Paginate(v, 5).size(), 3u);
+	ASSERT_EQUAL(Paginate(v, 4).size(), 4u);
+	ASSERT_EQUAL(Paginate(v, 15).size(), 1u);
+	ASSERT_EQUAL(Paginate(v, 150).size(), 1u);
+	ASSERT_EQUAL(Paginate(v, 14).size(), 2u);
+}
+
+void TestLooping() {
+	vector<int> v(15);
+	iota(begin(v), end(v), 1);
+
+	Paginator<vector<int>::iterator> paginate_v(v.begin(), v.end(), 6);
+	ostringstream os;
+	for (const auto& page : paginate_v) {
+		for (int x : page) {
+			os << x << ' ';
+		}
+		os << '\n';
+	}
+
+	ASSERT_EQUAL(os.str(), "1 2 3 4 5 6 \n7 8 9 10 11 12 \n13 14 15 \n");
+}
+
+void TestModification() {
+	vector<string> vs = { "one", "two", "three", "four", "five" };
+	for (auto page : Paginate(vs, 2)) {
+		for (auto& word : page) {
+			word[0] = toupper(word[0]);
+		}
+	}
+
+	const vector<string> expected = { "One", "Two", "Three", "Four", "Five" };
+	ASSERT_EQUAL(vs, expected);
+}
+
+void TestPageSizes() {
+	string letters(26, ' ');
+
+	Paginator letters_pagination(letters.begin(), letters.end(), 11);
+	vector<size_t> page_sizes;
+	for (const auto& page : letters_pagination) {
+		page_sizes.push_back(page.size());
+	}
+
+	const vector<size_t> expected = { 11, 11, 4 };
+	ASSERT_EQUAL(page_sizes, expected);
+}
+
+void TestConstContainer() {
+	const string letters = "abcdefghijklmnopqrstuvwxyz";
+
+	vector<string> pages;
+	for (const auto& page : Paginate(letters, 10)) {
+		pages.push_back(string(page.begin(), page.end()));
+	}
+
+	const vector<string> expected = { "abcdefghij", "klmnopqrst", "uvwxyz" };
+	ASSERT_EQUAL(pages, expected);
+}
+
+void TestPagePagination() {
+	vector<int> v(22);
+	iota(begin(v), end(v), 1);
+
+	vector<vector<int>> lines;
+	for (const auto& split_by_9 : Paginate(v, 9)) {
+		for (const auto& split_by_4 : Paginate(split_by_9, 4)) {
+			lines.push_back({});
+			for (int item : split_by_4) {
+				lines.back().push_back(item);
+			}
+		}
+	}
+
+	const vector<vector<int>> expected = {
+		{ 1, 2, 3, 4 },
+		{ 5, 6, 7, 8 },
+		{ 9 },
+		{ 10, 11, 12, 13 },
+		{ 14, 15, 16, 17 },
+		{ 18 },
+		{ 19, 20, 21, 22 }
+	};
+	ASSERT_EQUAL(lines, expected);
+}
+
 int main() {
-	//vector<int> vi;
-	//cout << vi.size() << endl;
+
+	//vector<int> vec{ 1,2,3,4,5 };
+	//vector<int> vec2(vec.begin(), vec.begin()+2);
 
 
-	//d.PushBack(4);
-	//d.PushFront(2);
-	//d.PushFront(1);
 
-	//int i = d.At(3);
-	//d.At(3) = 5;
-	//d[2] = 6;
-	//d[1] = 7;
-	//const int v1 = d[2];
-	//const int v2 = d.At(2);
-	//int v3 = d.Front();
-	//const int v4 = d.Front();
-
-	//cout << d.At(1) << endl;
-
-	//for (int i : {0, 1, 2, 3}) {
-	//	cout << d.At(i) << " ";
-	//}
-	//cout << endl;
-
-	//cout << d.Back() << endl;
-	//cout << d.Front() << endl;
-	
+	TestRunner tr;
+	RUN_TEST(tr, TestPageCounts);
+	RUN_TEST(tr, TestLooping);
+	RUN_TEST(tr, TestModification);
+	RUN_TEST(tr, TestPageSizes);
+	RUN_TEST(tr, TestConstContainer);
+	RUN_TEST(tr, TestPagePagination);	
 
 
 #ifdef _MSC_VER
