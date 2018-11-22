2ce3215e465f430d0a13f7c4cbc501db82012590
BerezinAlexander <elmario2pro@gmail.com>
2018-11-22 14:26:06 +0300

    Дек на основе двух векторов

--- coursera_ya/coursera_ya.cpp
+++ coursera_ya/coursera_ya.cpp
@@ -1,55 +1,138 @@
-#include "test_runner.h"
-
 #include <iostream>
 #include <string>
 #include <vector>
+#include <exception>
 
 using namespace std;
 
 template <typename T>
-class Table {
+class Deque {
 public:
-	Table(size_t col, size_t row) {
-		Resize(col, row);
+	Deque() = default;
+
+	bool Empty() const {
+		return vFront.empty() && vBack.empty();
 	}
 
-	vector<T>& operator[](const size_t num) {
-		return table[num];
+	size_t Size() const {
+		return (Empty()) ? 0 : (vFront.size() + vBack.size());
 	}
 
-	const vector<T>& operator[](const size_t num) const {
-		return table[num];
+	T& operator[](size_t index) {
+		return At(index);
 	}
 
-	void Resize(size_t col, size_t row) {
-		table.resize(col);
-		for (auto& vec_r : table) {
-			vec_r.resize(row);
+	const T& operator[](size_t index) const {
+		return At(index);
+	}
+
+	T& At(size_t index) {
+		if (index > (Size() - 1))
+			throw out_of_range("");
+
+		size_t pos = index;
+		if (pos >= vFront.size()) {
+			pos -= vFront.size();
+			return vBack.at(pos);
+		}
+		else {
+			return vFront.at(vFront.size() - 1 - pos);//   *(next(vFront.rbegin(), pos));
 		}
 	}
 
-	pair<size_t, size_t> Size() const {
-		return{ table.size(), (table.empty()) ? 0 : table.begin()->size() };
+	const T& At(size_t index) const {
+		if (index >= Size())
+			throw out_of_range("");
+
+		size_t pos = index;
+		if (pos >= vFront.size()) {
+			pos -= vFront.size();
+			return vBack.at(pos);
+		}
+		else {
+			return vFront.at(vFront.size() - 1 - pos);//   *(next(vFront.rbegin(), pos));
+		}
+	}
+
+	T& Front() {
+		if (Empty())
+			throw out_of_range("");
+		else if (!vFront.empty())
+			return vFront.back();
+		else
+			return vBack.front();
+	}
+
+
+	const T& Front() const {
+		if(Empty())
+			throw out_of_range("");
+		else if (!vFront.empty()) 
+			return vFront.back();
+		else 
+			return vBack.front();
+	}
+
+	T& Back() {
+		if (Empty())
+			throw out_of_range("");
+		else if (!vBack.empty())
+			return vBack.back();
+		else
+			return vFront.front();
+	}
+
+	const T& Back() const {
+		if (Empty())
+			throw out_of_range("");
+		else if (!vBack.empty())
+			return vBack.back();
+		else
+			return vFront.front();
+	}
+
+	void PushBack(const T& val) {
+		vBack.emplace_back(val);
+	}
+
+	void PushFront(const T& val) {
+		vFront.emplace_back(val);
 	}
 
 private:
-	vector<vector<T>> table;
+	vector<T> vFront;
+	vector<T> vBack;
 };
 
-void TestTable() {
-	Table<int> t(1, 1);
-	ASSERT_EQUAL(t.Size().first, 1u);
-	ASSERT_EQUAL(t.Size().second, 1u);
-	t[0][0] = 42;
-	ASSERT_EQUAL(t[0][0], 42);
-	t.Resize(3, 4);
-	ASSERT_EQUAL(t.Size().first, 3u);
-	ASSERT_EQUAL(t.Size().second, 4u);
-}
-
 int main() {
-	TestRunner tr;
-	RUN_TEST(tr, TestTable);
+	//vector<int> vi;
+	//cout << vi.size() << endl;
+
+
+	//d.PushBack(4);
+	//d.PushFront(2);
+	//d.PushFront(1);
+
+	//int i = d.At(3);
+	//d.At(3) = 5;
+	//d[2] = 6;
+	//d[1] = 7;
+	//const int v1 = d[2];
+	//const int v2 = d.At(2);
+	//int v3 = d.Front();
+	//const int v4 = d.Front();
+
+	//cout << d.At(1) << endl;
+
+	//for (int i : {0, 1, 2, 3}) {
+	//	cout << d.At(i) << " ";
+	//}
+	//cout << endl;
+
+	//cout << d.Back() << endl;
+	//cout << d.Front() << endl;
+	
+
 
 #ifdef _MSC_VER
 	system("pause");
