782b6e8dea53f9218484eb8cc36b6e10d109c738
BerezinAlexander <elmario2pro@gmail.com>
2019-04-08 10:03:55 +0300

    work slow version

--- upgrate/upgrate/main.cpp
+++ upgrate/upgrate/main.cpp
@@ -3,60 +3,93 @@
 #include <vector>
 #include <utility>
 #include <map>
+#include <string>
+#include <set>
 
 using namespace std;
 
-class ReadingManager {
-public:
-	ReadingManager()
-		// -1 значит, что не случилось ни одного READ
-		: user_page_counts_(MAX_USER_COUNT_ + 1, -1),
-		page_achieved_by_count_(MAX_PAGE_COUNT_ + 1, 0) {}
+static double current_time = 0;
 
-	void Read(int user_id, int page_count) {
-		UpdatePageRange(user_page_counts_[user_id] + 1, page_count + 1);
-		user_page_counts_[user_id] = page_count;
+struct Record {
+	double time;
+	int client_id;
+	int room_count;
+
+	Record(const double& time_, const int& client_id_,
+		const int& room_count_)
+		: time(time_), client_id(client_id_), room_count(room_count_)
+	{}
+};
+
+class HotelManager {
+public:
+	HotelManager() = default;
+	~HotelManager() = default;
+
+	void reservation(const double& time, const int& client_id, 
+		const int& room_count)
+	{
+		records.emplace_back(time, client_id, room_count);
 	}
 
-	double Cheer(int user_id) const {
-		const int pages_count = user_page_counts_[user_id];
-		if (pages_count == -1) {
-			return 0;
+	size_t clients() {
+		set<int> clients;
+		for (auto rit = records.rbegin(); rit != records.rend(); ++rit) {
+			if (current_time - 86400 < rit->time) {
+				clients.insert(rit->client_id);
+			}
 		}
-		const int user_count = GetUserCount();
-		if (user_count == 1) {
-			return 1;
+		return clients.size();
+	}
+
+	int rooms() {
+		int rooms_count = 0;
+		for (auto rit = records.rbegin(); rit != records.rend(); ++rit) {
+			if (current_time - 86400 < rit->time) {
+				rooms_count += rit->room_count;
+			}
 		}
-		// ѕо умолчанию деление целочисленное, поэтому
-		// нужно привести числитель к типу double.
-		// ѕростой способ сделать это Ч умножить его на 1.0.
-		return (user_count - page_achieved_by_count_[pages_count]) * 1.0
-			/ (user_count - 1);
+		return rooms_count;
 	}
 
 private:
-	// —татическое поле не принадлежит какому-либо конкретному объекту класса. 
-	// ѕо сути это глобальна€ переменна€, в данном случае - константна€.
-	// Ѕудь она публичной, к ней можно было бы обратитьс€ снаружи
-	// следующим образом: ReadingManager::MAX_USER_COUNT.
-	static const int MAX_USER_COUNT_ = 100'000;
-	static const int MAX_PAGE_COUNT_ = 1'000;
+	vector<Record> records;
+	//map<int, int> cl_rooms;
+};
 
-	// Ќомер страницы, до которой дочитал пользователь <ключ>
-	vector<int> user_page_counts_;
-	//  оличество пользователей, дочитавших (как минимум) до страницы <индекс>
-	vector<int> page_achieved_by_count_;
+class ReservationManager {
+public:
+	ReservationManager() = default;
+	~ReservationManager() = default;
 
-	int GetUserCount() const {
-		return page_achieved_by_count_[0];
-	}
-
-	// lhs включительно, rhs не включительно
-	void UpdatePageRange(int lhs, int rhs) {
-		for (int i = lhs; i < rhs; ++i) {
-			++page_achieved_by_count_[i];
+	void book(const double& time, const string& hotel_name,
+		const int& client_id, const int& room_count)
+	{
+		if (hotels.count(hotel_name) == 0) {
+			hotels[hotel_name] = HotelManager();
 		}
+		
+		hotels[hotel_name].reservation(time, client_id, room_count);
 	}
+
+	size_t clients(const string& hotel_name) {
+		if (hotels.count(hotel_name) == 0) {
+			return 0;
+		}
+
+		return hotels[hotel_name].clients();
+	}
+
+	int rooms(const string& hotel_name)	{
+		if (hotels.count(hotel_name) == 0) {
+			return 0;
+		}
+
+		return hotels[hotel_name].rooms();
+	}
+
+private:
+	map<string, HotelManager> hotels;
 };
 
 
@@ -87,24 +120,37 @@ int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(nullptr);
 
-	ReadingManager manager;
+	ReservationManager manager;
 
-	int query_count = 43;
+	int query_count;
 	cin >> query_count;
 
 	for (int query_id = 0; query_id < query_count; ++query_id) {
 		string query_type;
 		cin >> query_type;
-		int user_id;
-		cin >> user_id;
 
-		if (query_type == "READ") {
-			int page_count;
-			cin >> page_count;
-			manager.Read(user_id, page_count);
+		if (query_type == "BOOK") {
+			double time;
+			string hotel_name;
+			int client_id;
+			int room_count;
+			cin >> time >> hotel_name >> client_id >> room_count;
+
+			current_time = time;
+
+			manager.book(time, hotel_name, client_id, room_count);
 		}
-		else if (query_type == "CHEER") {
-			cout << setprecision(6) << manager.Cheer(user_id) << "\n";
+		else if (query_type == "CLIENTS") {
+			string hotel_name;
+			cin >> hotel_name;
+
+			cout << manager.clients(hotel_name) << "\n";
+		}
+		else if (query_type == "ROOMS") {
+			string hotel_name;
+			cin >> hotel_name;
+
+			cout << manager.rooms(hotel_name) << "\n";
 		}
 	}
 
