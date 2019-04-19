805c57838cadcadaea80ccbc270a7324d4d10eea
BerezinAlexander <elmario2pro@gmail.com>
2019-04-19 18:00:00 +0300

    Blog parsing (start version)

--- upgrate/upgrate/main.cpp
+++ upgrate/upgrate/main.cpp
@@ -24,104 +24,80 @@
 
 using namespace std;
 
-template <typename Iterator>
-class IteratorRange {
-public:
-	IteratorRange(Iterator begin, Iterator end)
-		: first(begin)
-		, last(end)
-		, size_(distance(first, last))
-	{
-	}
+inline void addToMap(map<string, int>& m, const string& word) {
+	m.count(word) ? m[word]++ : m[word] = 1;
+}
 
-	Iterator begin() const {
-		return first;
-	}
+struct Stats {
+	map<string, int> word_frequences;
 
-	Iterator end() const {
-		return last;
-	}
-
-	size_t size() const {
-		return size_;
-	}
-
-private:
-	Iterator first, last;
-	size_t size_;
-};
-
-template <typename Iterator>
-class Paginator {
-private:
-	vector<IteratorRange<Iterator>> pages;
-
-public:
-	Paginator(Iterator begin, Iterator end, size_t page_size) {
-		for (size_t left = distance(begin, end); left > 0; ) {
-			size_t current_page_size = min(page_size, left);
-			Iterator current_page_end = next(begin, current_page_size);
-			pages.push_back({ begin, current_page_end });
-
-			left -= current_page_size;
-			begin = current_page_end;
+	void operator += (const Stats& other) {
+		for (auto& p : other.word_frequences) {
+			word_frequences.count(p.first) ? word_frequences[p.first] += p.second 
+										   : word_frequences[p.first] = p.second;
 		}
-	}
-
-	auto begin() const {
-		return pages.begin();
-	}
-
-	auto end() const {
-		return pages.end();
-	}
-
-	size_t size() const {
-		return pages.size();
 	}
 };
 
-template <typename C>
-auto Paginate(C& c, size_t page_size) {
-	return Paginator(begin(c), end(c), page_size);
-}
-
-template <typename ContainerOfVectors>
-int64_t SumSingleThread(const ContainerOfVectors& matrix) {
-	int64_t sum = 0;
-	for (const auto& row : matrix) {
-		for (auto item : row) {
-			sum += item;
+Stats ExploreLine(const set<string>& key_words, const string& line) {
+	stringstream ss(line);
+	Stats stats;
+	string word;
+	while (ss >> word) {
+		if (key_words.count(word)) {
+			addToMap(stats.word_frequences, word);
 		}
 	}
-	return sum;
+	return stats;
 }
 
-int64_t CalculateMatrixSum(const vector<vector<int>>& matrix) {
-	vector<future<int64_t>> futures;
-	for (auto page : Paginate(matrix, 2000)) {
-		futures.push_back(async([=] { return SumSingleThread(page); }));
-	}
-	int64_t result = 0;
-	for (auto& f : futures) {
-		result += f.get();
+Stats ExploreKeyWordsSingleThread(
+	const set<string>& key_words, istream& input
+) {
+	Stats result;
+	for (string line; getline(input, line); ) {
+		result += ExploreLine(key_words, line);
 	}
 	return result;
 }
 
-void TestCalculateMatrixSum() {
-	const vector<vector<int>> matrix = {
-	  {1, 2, 3, 4},
-	  {5, 6, 7, 8},
-	  {9, 10, 11, 12},
-	  {13, 14, 15, 16}
+Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
+	// Реализуйте эту функцию
+	vector<future<Stats>> futures;
+	for (string line; getline(input, line); ) {
+		futures.push_back( async(ExploreLine, ref(key_words), ref(line)) );
+	}
+
+	Stats stats;
+	for (auto& f : futures) {
+		stats += f.get();
+	}
+
+	return stats;
+}
+
+void TestBasic() {
+	const set<string> key_words = { "yangle", "rocks", "sucks", "all" };
+
+	stringstream ss;
+	ss << "this new yangle service really rocks\n";
+	ss << "It sucks when yangle isn't available\n";
+	ss << "10 reasons why yangle is the best IT company\n";
+	ss << "yangle rocks others suck\n";
+	ss << "Goondex really sucks, but yangle rocks. Use yangle\n";
+
+	const auto stats = ExploreKeyWords(key_words, ss);
+	const map<string, int> expected = {
+	  {"yangle", 6},
+	  {"rocks", 2},
+	  {"sucks", 1}
 	};
-	ASSERT_EQUAL(CalculateMatrixSum(matrix), 136);
+	ASSERT_EQUAL(stats.word_frequences, expected);
 }
 
 int main() {
 	TestRunner tr;
-	RUN_TEST(tr, TestCalculateMatrixSum);
+	RUN_TEST(tr, TestBasic);
 
 #ifdef _MSC_VER
 	system("pause");
