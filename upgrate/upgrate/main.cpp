0e9d30942c7dba4389dd2ae113dad957dc55146b
elmario <elmario2pro@gmail.com>
2019-04-20 16:52:35 +0300

    Blog parsing (637 ms in average, but limit is 450 ms)

--- upgrate/upgrate/main.cpp
+++ upgrate/upgrate/main.cpp
@@ -26,76 +26,6 @@
 
 using namespace std;
 
-
-
-template <typename Iterator>
-class IteratorRange {
-public:
-	IteratorRange(Iterator begin, Iterator end)
-		: first(begin)
-		, last(end)
-		, size_(distance(first, last))
-	{
-	}
-
-	Iterator begin() const {
-		return first;
-	}
-
-	Iterator end() const {
-		return last;
-	}
-
-	size_t size() const {
-		return size_;
-	}
-
-private:
-	Iterator first, last;
-	size_t size_;
-};
-
-template <typename Iterator>
-class Paginator {
-private:
-	vector<IteratorRange<Iterator>> pages;
-
-public:
-	Paginator(Iterator begin, Iterator end, size_t page_size) {
-		for (size_t left = distance(begin, end); left > 0; ) {
-			size_t current_page_size = min(page_size, left);
-			Iterator current_page_end = next(begin, current_page_size);
-			pages.push_back({ begin, current_page_end });
-
-			left -= current_page_size;
-			begin = current_page_end;
-		}
-	}
-
-	auto begin() const {
-		return pages.begin();
-	}
-
-	auto end() const {
-		return pages.end();
-	}
-
-	size_t size() const {
-		return pages.size();
-	}
-};
-
-template <typename C>
-auto Paginate(C& c, size_t page_size) {
-	return Paginator(begin(c), end(c), page_size);
-}
-
-
-
-
-
-
-
 inline void addToMap(map<string, int>& m, const string& word) {
 	m.count(word) ? m[word]++ : m[word] = 1;
 }
@@ -133,17 +63,13 @@ Stats ExploreLine(const set<string>& key_words, const string& line) {
 	return stats;
 }
 
-template <typename ContainerOfVectors>
 Stats ExploreKeyWordsSingleThread(
-	const set<string>& key_words, ContainerOfVectors& strings
+	const set<string>& key_words, istream& input
 ) {
 	Stats result;
-	for (auto& s : strings) {
-		result += ExploreLine(key_words, s);
+	for (string line; getline(input, line); ) {
+		result += ExploreLine(key_words, line);
 	}
-	//for (string line; getline(input, line); ) {
-	//	result += ExploreLine(key_words, line);
-	//}
 	return result;
 }
 
@@ -151,43 +77,18 @@ Stats ExploreKeyWords(const set<string>& key_words, istream& input) {
 	
 	LOG_DURATION("ExploreKeyWords");
 
-
-	vector<string> strings;
-	{
-		LOG_DURATION("Fill");
-		for (string line; getline(input, line); ) {
-			strings.push_back(move(line));
-		}
-		cout << endl;
-	}
-
+	// Реализуйте эту функцию
 	vector<future<Stats>> futures;
-	Stats result;
-	{
-		LOG_DURATION("Calc");
-		for (auto page : Paginate(strings, 1000)) {
-			futures.push_back(async([=] { return ExploreKeyWordsSingleThread(key_words, page); }));
-		}
-		for (auto& f : futures) {
-			result += f.get();
-		}
-		cout << endl;
+	for (string line; getline(input, line); ) {
+		futures.push_back( async(ExploreLine, ref(key_words), line) );
 	}
 
-	//// Реализуйте эту функцию
-	//vector<future<Stats>> futures;
-	//for (string line; getline(input, line); ) {
-	//	futures.push_back( async(ExploreLine, ref(key_words), line) );
-	//}
+	Stats stats;
+	for (auto& f : futures) {
+		stats += f.get();
+	}
 
-	//Stats stats;
-	//for (auto& f : futures) {
-	//	stats += f.get();
-	//}
-
-	cout << endl;
-
-	return result;
+	return stats;
 }
 
 void TestBasic() {
@@ -219,7 +120,7 @@ void TestBasic() {
 void TestBasic2() {
 	const set<string> key_words = { "yangle", "rocks", "sucks", "all" };
 
-	const int max_count = 50000;
+	const int max_count = 1000;
 
 	stringstream ss;
 	for (int i = 0; i < max_count; ++i) {
@@ -230,8 +131,6 @@ void TestBasic2() {
 	}
 
 	const auto stats = ExploreKeyWords(key_words, ss);
-
-	cout << endl;
 
 	//cout << stats << endl;
 	//cout << stats.word_frequences << endl << endl;
