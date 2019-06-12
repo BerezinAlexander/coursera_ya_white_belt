118aada5e9f404e39aa9e63abe1210cea92d63b8
BerezinAlexander <elmario2pro@gmail.com>
2019-06-12 16:26:24 +0300

    Интеграция структуры Point в unordered_set

--- upgrate/upgrate/main.cpp
+++ upgrate/upgrate/main.cpp
@@ -1,157 +1,190 @@
 #include "test_runner.h"
-#include <cassert>
-#include <deque>
-#include <iostream>
+#include <limits>
+#include <random>
+#include <unordered_set>
 
 using namespace std;
 
+using CoordType = int;
 
-struct Node {
-	Node(int v, Node* p)
-		: value(v)
-		, parent(p)
-	{}
+struct Point3D {
+	CoordType x;
+	CoordType y;
+	CoordType z;
 
-	int value;
-	Node* left = nullptr;
-	Node* right = nullptr;
-	Node* parent;
+	bool operator==(const Point3D& other) const {
+		//return Hasher(*this) == Hasher(other);
+		return (x == other.x && y == other.y && z == other.z);
+	}
 };
 
-
-class NodeBuilder {
-public:
-	Node* CreateRoot(int value) {
-		nodes.emplace_back(value, nullptr);
-		return &nodes.back();
+struct Hasher {
+	size_t operator() (const Point3D& crd) const {
+		size_t a = ctHash(crd.x);
+		size_t b = ctHash(crd.y);
+		size_t c = ctHash(crd.z);
+		size_t x = 27644437;
+		return a * x*x + b * x + c;
 	}
 
-	Node* CreateLeftSon(Node* me, int value) {
-		assert(me->left == nullptr);
-		nodes.emplace_back(value, me);
-		me->left = &nodes.back();
-		return me->left;
-	}
-
-	Node* CreateRightSon(Node* me, int value) {
-		assert(me->right == nullptr);
-		nodes.emplace_back(value, me);
-		me->right = &nodes.back();
-		return me->right;
-	}
-
-private:
-	deque<Node> nodes;
+	hash<CoordType> ctHash;
 };
 
-Node* GetLeftest(Node* me) {
-	assert(me);
-	while (me->left) {
-		me = me->left;
-	}
-	return me;
-}
+void TestSmoke() {
+	vector<Point3D> points = {
+	  {1, 2, 3},
+	  {0, 2, 3},
+	  {1, 0, 3},
+	  {1, 2, 0},
+	};
 
-
-Node* Next(Node* me) {
-	assert(me);
-
-	if (me->right) {
-		return GetLeftest(me->right);
+	unordered_set<Point3D, Hasher> point_set;
+	for (const auto& point : points) {
+		point_set.insert(point);
 	}
 
-	if (me->parent && me->parent->left == me) {
-		return me->parent;
-	}
-
-	while (me->parent && me == me->parent->right) {
-		me = me->parent;
-	}
-	return me->parent;
-}
-
-//Node* findMinLeft(Node* me) {
-//	Node* cur = me;
-//	while (cur->left)
-//		cur = cur->left;
-//	return cur;
-//}
-//
-//Node* findParentWithRight(Node* me) {
-//	Node* parent = me->parent;
-//	Node* cur = me;
-//
-//	if (parent->left == cur)
-//		return parent;
-//
-//	while (parent->right == cur) {
-//		if (!parent->parent)
-//			return nullptr;
-//		cur = parent;
-//		parent = parent->parent;
-//	}
-//
-//	return parent;
-//}
-//
-//
-//Node* Next(Node* me) {
-//	if (!me->right) {
-//		if (me->parent)
-//			return findParentWithRight(me);
-//		else
-//			return nullptr;
-//	}
-//	else if (me->right) {
-//		return findMinLeft(me->right);
-//	}
-//}
-
-
-void Test1() {
-	NodeBuilder nb;
-
-	Node* root = nb.CreateRoot(50);
-	ASSERT_EQUAL(root->value, 50);
-
-	Node* l = nb.CreateLeftSon(root, 2);
-	Node* min = nb.CreateLeftSon(l, 1);
-	Node* r = nb.CreateRightSon(l, 4);
-	ASSERT_EQUAL(min->value, 1);
-	ASSERT_EQUAL(r->parent->value, 2);
-
-	nb.CreateLeftSon(r, 3);
-	nb.CreateRightSon(r, 5);
-
-	r = nb.CreateRightSon(root, 100);
-	l = nb.CreateLeftSon(r, 90);
-	nb.CreateRightSon(r, 101);
-
-	nb.CreateLeftSon(l, 89);
-	r = nb.CreateRightSon(l, 91);
-
-	ASSERT_EQUAL(Next(l)->value, 91);
-	ASSERT_EQUAL(Next(root)->value, 89);
-	ASSERT_EQUAL(Next(min)->value, 2);
-	ASSERT_EQUAL(Next(r)->value, 100);
-
-	while (min) {
-		cout << min->value << '\n';
-		min = Next(min);
+	ASSERT_EQUAL(points.size(), point_set.size());
+	for (const auto& point : points) {
+		ASSERT_EQUAL(point_set.count(point), static_cast<size_t>(1));
 	}
 }
 
-void TestRootOnly() {
-	NodeBuilder nb;
-	Node* root = nb.CreateRoot(42);
-	ASSERT(Next(root) == nullptr);
+void TestPurity() {
+	Point3D point = { 1, 2, 3 };
+	Hasher hasher;
+
+	auto hash = hasher(point);
+	for (size_t t = 0; t < 100; ++t) {
+		ASSERT_EQUAL(hasher(point), hash);
+	}
 };
 
+void TestX() {
+	auto seed = 42;
+	mt19937 gen(seed);
+	uniform_int_distribution<CoordType> dist(
+		numeric_limits<CoordType>::min(),
+		numeric_limits<CoordType>::max()
+	);
+
+	Hasher hasher;
+
+	bool component_matters = false;
+	for (size_t t = 0; t < 1000; ++t) {
+		Point3D point = { dist(gen), dist(gen), dist(gen) };
+		Point3D other_point = point;
+		other_point.x = dist(gen);
+
+		component_matters |= (hasher(point) != hasher(other_point));
+	}
+
+	ASSERT(component_matters);
+}
+
+void TestY() {
+	auto seed = 42;
+	mt19937 gen(seed);
+	uniform_int_distribution<CoordType> dist(
+		numeric_limits<CoordType>::min(),
+		numeric_limits<CoordType>::max()
+	);
+
+	Hasher hasher;
+
+	bool component_matters = false;
+	for (size_t t = 0; t < 1000; ++t) {
+		Point3D point = { dist(gen), dist(gen), dist(gen) };
+		Point3D other_point = point;
+		other_point.y = dist(gen);
+
+		component_matters |= (hasher(point) != hasher(other_point));
+	}
+
+	ASSERT(component_matters);
+}
+
+void TestZ() {
+	auto seed = 42;
+	mt19937 gen(seed);
+	uniform_int_distribution<CoordType> dist(
+		numeric_limits<CoordType>::min(),
+		numeric_limits<CoordType>::max()
+	);
+
+	Hasher hasher;
+
+	bool component_matters = false;
+	for (size_t t = 0; t < 1000; ++t) {
+		Point3D point = { dist(gen), dist(gen), dist(gen) };
+		Point3D other_point = point;
+		other_point.z = dist(gen);
+
+		component_matters |= (hasher(point) != hasher(other_point));
+	}
+
+	ASSERT(component_matters);
+}
+
+void TestDistribution() {
+	auto seed = 42;
+	mt19937 gen(seed);
+	uniform_int_distribution<CoordType> dist(
+		numeric_limits<CoordType>::min(),
+		numeric_limits<CoordType>::max()
+	);
+
+	Hasher hasher;
+
+	// выбираем число бакетов не очень большим простым числом
+	// (unordered_set, unordered_map используют простые числа бакетов
+	// в реализаци€х GCC и Clang, дл€ непростых чисел бакетов
+	// возникают сложности со стандартной хеш-функцией в этих реализаци€х)
+	const size_t num_buckets = 2053;
+
+	// мы хотим, чтобы число точек в бакетах было ~100'000
+	const size_t perfect_bucket_size = 50;
+	const size_t num_points = num_buckets * perfect_bucket_size;
+	vector<size_t> buckets(num_buckets);
+	for (size_t t = 0; t < num_points; ++t) {
+		Point3D point = { dist(gen), dist(gen), dist(gen) };
+		++buckets[hasher(point) % num_buckets];
+	}
+
+	// —татистика ѕирсона:
+	// https://en.wikipedia.org/wiki/Pearson's_chi-squared_test
+	//
+	// „исленной мерой равномерности распределени€ также может выступать
+	// энтропи€, но дл€ ее порогов нет хорошей статистической интерпретации
+	double pearson_stat = 0;
+	for (auto bucket_count : buckets) {
+		size_t count_diff = bucket_count - perfect_bucket_size;
+		pearson_stat +=
+			count_diff * count_diff / static_cast<double>(perfect_bucket_size);
+	}
+
+	// провер€ем равномерность распределени€ методом согласи€ ѕирсона
+	// со статистической значимостью 0.95:
+	//
+	// если подставить вместо ++buckets[hasher(point) % num_buckets]
+	// выражение ++buckets[dist(gen) % num_buckets], то с веро€тностью 95%
+	// ASSERT ниже отработает успешно,
+	//
+	// т.к. статистика ѕирсона приблизительно распределена по chi^2
+	// с числом степеней свободы, равным num_buckets - 1,
+	// и 95 процентиль этого распределени€ равен:
+	// >>> scipy.stats.chi2.ppf(0.95, 2052)
+	const double critical_value = 2158.4981036918693;
+	ASSERT(pearson_stat < critical_value);
+}
 
 int main() {
 	TestRunner tr;
-	RUN_TEST(tr, Test1);
-	RUN_TEST(tr, TestRootOnly);
+	RUN_TEST(tr, TestSmoke);
+	RUN_TEST(tr, TestPurity);
+	RUN_TEST(tr, TestX);
+	RUN_TEST(tr, TestY);
+	RUN_TEST(tr, TestZ);
+	RUN_TEST(tr, TestDistribution);
 
 #ifdef _MSC_VER
 	system("pause");
