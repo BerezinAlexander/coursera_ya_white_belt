c017b64b075e625b59ac7af7d1e835af3961ea4e
BerezinAlexander <elmario2pro@gmail.com>
2019-06-13 13:50:55 +0300

    Вторичный индекс в базе данных

--- upgrate/upgrate/main.cpp
+++ upgrate/upgrate/main.cpp
@@ -1,163 +1,151 @@
 #include "test_runner.h"
-#include "profile.h"
 
-#include <cstdint>
-#include <iterator>
-#include <numeric>
-
-#include <algorithm>
-#include <array>
 #include <iostream>
-#include <random>
-#include <vector>
-#include <list>
-#include <deque>
-
-#include <string_view>
-#include <tuple>
-#include <memory>
-#include <iterator>
-#include <forward_list>
+#include <map>
+#include <string>
+#include <unordered_map>
 
 using namespace std;
 
-template <typename Type, typename Hasher = hash<Type>>
-class HashSet {
+struct Record {
+	string id;
+	string title;
+	string user;
+	int timestamp;
+	int karma;
+};
+
+// Реализуйте этот класс
+class Database {
 public:
-	using BucketList = forward_list<Type>;
+	bool Put(const Record& record) {
+		if (!GetById(record.id)) {
+			records[record.id] = record;
+			Iters iters;
+			iters.iter_timestamp = timestamp_records.insert(pair(record.timestamp, &records[record.id]));
+			iters.iter_karma = karma_records.insert(pair(record.karma, &records[record.id]));
+			iters.iter_user = user_records.insert(pair(record.user, &records[record.id]));
+			id_iters[record.id] = move(iters);
+			return true;
+		}
+		return false;
+	}
 
-public:
-	explicit HashSet(
-		size_t num_buckets,
-		const Hasher& hasher = {}
-	)
-		: hasher_(hasher)
-		, buckets_(num_buckets)
-	{}
+	const Record* GetById(const string& id) const
+	{
+		return (!records.count(id)) ? nullptr : &records.at(id);
+	}
+	
+	bool Erase(const string& id)
+	{
+		if (GetById(id)) {
+			Iters& iters = id_iters[id];
+			timestamp_records.erase(iters.iter_timestamp);
+			karma_records.erase(iters.iter_karma);
+			user_records.erase(iters.iter_user);
+			id_iters.erase(id);
+			records.erase(id);
+			return true;
+		}
+		return false;
+	}
 
-	void Add(const Type& value) {
-		auto& bucket = buckets_[GetBucketIndex(value)];
-		auto it = find(begin(bucket), end(bucket), value);
-
-		if (it == bucket.end()) {
-			bucket.push_front(value);
+	template <typename Callback>
+	void RangeByTimestamp(int low, int high, Callback callback) const {
+		auto itBegin = timestamp_records.lower_bound(low);
+		auto itEnd = timestamp_records.upper_bound(high);
+		for (auto it = itBegin; it != itEnd; ++it) {
+			if (!callback(*(it->second)))
+				return;
 		}
 	}
 
-	bool Has(const Type& value) const {
-		const auto& bucket = GetBucket(value);
-		return find(begin(bucket), end(bucket), value) != bucket.end();
+	template <typename Callback>
+	void RangeByKarma(int low, int high, Callback callback) const {
+		auto itBegin = karma_records.lower_bound(low);
+		auto itEnd = karma_records.upper_bound(high);
+		for (auto it = itBegin; it != itEnd; ++it) {
+			if (!callback(*(it->second)))
+				return;
+		}
 	}
 
-	void Erase(const Type& value) {
-		buckets_[GetBucketIndex(value)].remove(value);
-	}
-
-	const BucketList& GetBucket(const Type& value) const {
-		return buckets_[GetBucketIndex(value)];
+	template <typename Callback>
+	void AllByUser(const string& user, Callback callback) const {
+		auto itBegin = user_records.lower_bound(user);
+		auto itEnd = user_records.upper_bound(user);
+		for (auto it = itBegin; it != itEnd; ++it) {
+			if (!callback(*(it->second)))
+				return;
+		}
 	}
 
 private:
-	size_t GetBucketIndex(const Type& value) const {
-		return hasher_(value) % buckets_.size();
-	}
+	using Iter1 = multimap<int, Record*>::iterator;
+	using Iter2 = multimap<string, Record*>::iterator;
 
-private:
-	Hasher hasher_;
-	vector<BucketList> buckets_;
+	map<string, Record> records;
+	multimap<int, Record*> timestamp_records;
+	multimap<int, Record*> karma_records;
+	multimap<string, Record*> user_records;
+
+	struct Iters {
+		Iter1 iter_timestamp;
+		Iter1 iter_karma;
+		Iter2 iter_user;
+	};
+
+	map<string, Iters> id_iters;
 };
 
-struct IntHasher {
-	size_t operator()(int value) const {
-		// Это реальная хеш-функция из libc++, libstdc++.
-		// Чтобы она работала хорошо, std::unordered_map
-		// использует простые числа для числа бакетов
-		return value;
-	}
-};
+void TestRangeBoundaries() {
+	const int good_karma = 1000;
+	const int bad_karma = -10;
 
-struct TestValue {
-	int value;
+	Database db;
+	db.Put({ "id1", "Hello there", "master", 1536107260, good_karma });
+	db.Put({ "id2", "O>>-<", "general2", 1536107260, bad_karma });
 
-	bool operator==(TestValue other) const {
-		return value / 2 == other.value / 2;
-	}
-};
+	int count = 0;
+	db.RangeByKarma(bad_karma, good_karma, [&count](const Record&) {
+		++count;
+		return true;
+		});
 
-struct TestValueHasher {
-	size_t operator()(TestValue value) const {
-		return value.value / 2;
-	}
-};
-
-void TestSmoke() {
-	HashSet<int, IntHasher> hash_set(2);
-	hash_set.Add(3);
-	hash_set.Add(4);
-
-	ASSERT(hash_set.Has(3));
-	ASSERT(hash_set.Has(4));
-	ASSERT(!hash_set.Has(5));
-
-	hash_set.Erase(3);
-
-	ASSERT(!hash_set.Has(3));
-	ASSERT(hash_set.Has(4));
-	ASSERT(!hash_set.Has(5));
-
-	hash_set.Add(3);
-	hash_set.Add(5);
-
-	ASSERT(hash_set.Has(3));
-	ASSERT(hash_set.Has(4));
-	ASSERT(hash_set.Has(5));
+	ASSERT_EQUAL(2, count);
 }
 
-void TestEmpty() {
-	HashSet<int, IntHasher> hash_set(10);
-	for (int value = 0; value < 10000; ++value) {
-		ASSERT(!hash_set.Has(value));
-	}
+void TestSameUser() {
+	Database db;
+	db.Put({ "id1", "Don't sell", "master", 1536107260, 1000 });
+	db.Put({ "id2", "Rethink life", "master", 1536107260, 2000 });
+
+	int count = 0;
+	db.AllByUser("master", [&count](const Record&) {
+		++count;
+		return true;
+		});
+
+	ASSERT_EQUAL(2, count);
 }
 
-void TestIdempotency() {
-	HashSet<int, IntHasher> hash_set(10);
-	hash_set.Add(5);
-	ASSERT(hash_set.Has(5));
-	hash_set.Add(5);
-	ASSERT(hash_set.Has(5));
-	hash_set.Erase(5);
-	ASSERT(!hash_set.Has(5));
-	hash_set.Erase(5);
-	ASSERT(!hash_set.Has(5));
-}
+void TestReplacement() {
+	const string final_body = "Feeling sad";
 
-void TestEquivalence() {
-	HashSet<TestValue, TestValueHasher> hash_set(10);
-	hash_set.Add(TestValue{ 2 });
-	hash_set.Add(TestValue{ 3 });
+	Database db;
+	db.Put({ "id", "Have a hand", "not-master", 1536107260, 10 });
+	db.Erase("id");
+	db.Put({ "id", final_body, "not-master", 1536107260, -10 });
 
-	ASSERT(hash_set.Has(TestValue{ 2 }));
-	ASSERT(hash_set.Has(TestValue{ 3 }));
-
-	const auto& bucket = hash_set.GetBucket(TestValue{ 2 });
-	const auto& three_bucket = hash_set.GetBucket(TestValue{ 3 });
-	ASSERT_EQUAL(&bucket, &three_bucket);
-
-	ASSERT_EQUAL(1, distance(begin(bucket), end(bucket)));
-	ASSERT_EQUAL(2, bucket.front().value);
+	auto record = db.GetById("id");
+	ASSERT(record != nullptr);
+	ASSERT_EQUAL(final_body, record->title);
 }
 
 int main() {
 	TestRunner tr;
-	RUN_TEST(tr, TestSmoke);
-	RUN_TEST(tr, TestEmpty);
-	RUN_TEST(tr, TestIdempotency);
-	RUN_TEST(tr, TestEquivalence);
-
-#ifdef _MSC_VER
-	system("pause");
-#endif
-
+	RUN_TEST(tr, TestRangeBoundaries);
+	RUN_TEST(tr, TestSameUser);
+	RUN_TEST(tr, TestReplacement);
 	return 0;
 }
