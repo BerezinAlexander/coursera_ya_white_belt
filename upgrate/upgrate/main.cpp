c81a56df3bee01b148de4d76939410646ed73146
BerezinAlexander <elmario2pro@gmail.com>
2019-08-12 08:14:08 +0300

    Конвейер (unwork)
    (PipelineBuilder: first element in pipeline should always be an instance of Reader)

--- upgrate/upgrate/main.cpp
+++ upgrate/upgrate/main.cpp
@@ -1,66 +1,240 @@
-#include "animals.h"
 #include "test_runner.h"
-
-#include <iostream>
+#include <functional>
+#include <memory>
+#include <sstream>
 #include <stdexcept>
 #include <string>
-#include <sstream>
 #include <vector>
-#include <memory>
 
 using namespace std;
 
-using Zoo = vector<unique_ptr<Animal>>;
 
-// Эта функция получает на вход поток ввода и читает из него описание зверей.
-// Если очередное слово этого текста - Tiger, Wolf или Fox, функция должна поместить соответствующего зверя в зоопарк.
-// В противном случае она должна прекратить чтение и сгенерировать исключение runtime_error.
-Zoo CreateZoo(istream& in) {
-	Zoo zoo;
-	string word;
-	while (in >> word) {
-		if (word == "Tiger") {
-			zoo.push_back(make_unique<Tiger>());
-		}
-		else if (word == "Wolf") {
-			zoo.push_back(make_unique<Wolf>());
-		}
-		else if (word == "Fox") {
-			zoo.push_back(make_unique<Fox>());
-		}
-		else {
-			throw runtime_error("Unknown animal!");
+struct Email {
+	string from;
+	string to;
+	string body;
+};
+
+
+class Worker {
+public:
+	virtual ~Worker() = default;
+	virtual void Process(unique_ptr<Email> email) = 0;
+	virtual void Run() {
+		// только первому worker-у в пайплайне нужно это имплементировать
+		throw logic_error("Unimplemented");
+	}
+
+protected:
+	// реализации должны вызывать PassOn, чтобы передать объект дальше
+	// по цепочке обработчиков
+	void PassOn(unique_ptr<Email> email) const {
+		if(next_worker)
+			next_worker->Process(move(email));
+	}
+
+public:
+	void SetNext(unique_ptr<Worker> next) {
+		next_worker = move(next);
+	}
+
+protected:
+	unique_ptr<Worker> next_worker;
+};
+
+
+class Reader : public Worker {
+public:
+	/*конструктор должен принимать входной поток (istream&), из которого нужно читать сообщения*/
+	Reader(istream& input_)
+		: input(input_.rdbuf())
+	{}
+
+	/*метод Run должен читать из входного потока все e-mail'ы и передавать дальше каждый из них. 
+	Один e-mail представляется во входном потоке полями from, to, body, 
+	каждое из которых находится на своей строке.
+	*/
+	void Run() {
+		while (input) {
+			unique_ptr<Email> ptrEmail = make_unique<Email>();
+
+			getline(input, ptrEmail->from);
+			if (!input) break;
+			getline(input, ptrEmail->to);
+			if (!input) break;
+			getline(input, ptrEmail->body);
+			Process(move(ptrEmail));
 		}
 	}
-	return zoo;
-}
 
-// Эта функция должна перебрать всех зверей в зоопарке в порядке их создания
-// и записать в выходной поток для каждого из них результат работы виртуальной функции voice.
-// Разделяйте голоса разных зверей символом перевода строки '\n'.
-void Process(const Zoo& zoo, ostream& out) {
-	for (const auto& animal : zoo) {
-		out << animal.get()->Voice() << "\n";
+	void Process(unique_ptr<Email> email) {
+		PassOn(move(email));
 	}
-}
 
-void TestZoo() {
-	istringstream input("Tiger Wolf Fox Tiger");
-	ostringstream output;
-	Process(CreateZoo(input), output);
+private:
+	istream input;
+};
 
-	const string expected =
-		"Rrrr\n"
-		"Wooo\n"
-		"Tyaf\n"
-		"Rrrr\n";
 
-	ASSERT_EQUAL(output.str(), expected);
+class Filter : public Worker {
+public:
+	using Function = function<bool(const Email&)>;
+
+public:
+	Filter(Function predicate_)
+		: predicate(predicate_)
+	{}
+
+	void Process(unique_ptr<Email> email) {
+		if (predicate(*email.get())) {
+			PassOn(move(email));
+		}
+	}
+
+private:
+	Function predicate;
+};
+
+
+class Copier : public Worker {
+public:
+	Copier(string sendTo_)
+		: sendTo(sendTo_)
+	{}
+
+	void Process(unique_ptr<Email> email) {
+		auto em = email.get();
+		unique_ptr<Email> email2;
+		if (em->to != sendTo) {
+			email2 = make_unique<Email>(Email({ em->from, sendTo, em->body }));
+		}
+		PassOn(move(email));
+		if(email2)
+			PassOn(move(email2));
+	}
+
+private:
+	string sendTo;
+};
+
+
+class Sender : public Worker {
+public:
+	Sender(ostream& output_)
+		: output(output_.rdbuf())
+	{}
+
+	void Process(unique_ptr<Email> email) {
+		auto em = email.get();
+		output << em->from << "\n";
+		output << em->to << "\n";
+		output << em->body << "\n";
+
+		PassOn(move(email));
+	}
+
+private:
+	ostream output;
+};
+
+
+// реализуйте класс
+class PipelineBuilder {
+public:
+	PipelineBuilder() = delete;
+	// добавляет в качестве первого обработчика Reader
+	explicit PipelineBuilder(istream& in) {
+		reader = make_unique<Reader>(in);
+	}
+
+	// добавляет новый обработчик Filter
+	PipelineBuilder& FilterBy(Filter::Function filter) {
+		workers.emplace_back(make_unique<Filter>(filter));
+		return *this;
+	}
+
+	// добавляет новый обработчик Copier
+	PipelineBuilder& CopyTo(string recipient) {
+		workers.emplace_back(make_unique<Copier>(recipient));
+		return *this;
+	}
+
+	// добавляет новый обработчик Sender
+	PipelineBuilder& Send(ostream& out) {
+		workers.emplace_back(make_unique<Sender>(out));
+		return *this;
+	}
+
+	// возвращает готовую цепочку обработчиков
+	unique_ptr<Worker> Build() {
+		unique_ptr<Worker> main_worker;
+		if (!workers.empty()) {
+			main_worker = move(workers.back());
+			workers.pop_back();
+			while (!workers.empty()) {			
+				unique_ptr<Worker> worker = move(workers.back());
+				workers.pop_back();
+				worker->SetNext(move(main_worker));
+				main_worker = move(worker);
+			}
+		}
+		reader->SetNext(move(main_worker));
+		return move(reader);
+	}
+
+private:
+	vector<unique_ptr<Worker>> workers;
+	unique_ptr<Worker> reader;
+};
+
+
+void TestSanity() {
+	string input = (
+		"erich@example.com\n"
+		"richard@example.com\n"
+		"Hello there\n"
+
+		"erich@example.com\n"
+		"ralph@example.com\n"
+		"Are you sure you pressed the right button?\n"
+
+		"ralph@example.com\n"
+		"erich@example.com\n"
+		"I do not make mistakes of that kind\n"
+		);
+	istringstream inStream(input);
+	ostringstream outStream;
+
+	PipelineBuilder builder(inStream);
+	builder.FilterBy([](const Email& email) {
+		return email.from == "erich@example.com";
+		});
+	builder.CopyTo("richard@example.com");
+	builder.Send(outStream);
+	auto pipeline = builder.Build();
+
+	pipeline->Run();
+
+	string expectedOutput = (
+		"erich@example.com\n"
+		"richard@example.com\n"
+		"Hello there\n"
+
+		"erich@example.com\n"
+		"ralph@example.com\n"
+		"Are you sure you pressed the right button?\n"
+
+		"erich@example.com\n"
+		"richard@example.com\n"
+		"Are you sure you pressed the right button?\n"
+		);
+
+	ASSERT_EQUAL(expectedOutput, outStream.str());
 }
 
 int main() {
 	TestRunner tr;
-	RUN_TEST(tr, TestZoo);
+	RUN_TEST(tr, TestSanity);
 
 
 #ifdef _MSC_VER
